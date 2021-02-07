42aecfa04dc0c0ecbfe929eaf86ec7999e526857
Stefan Pejcic <pejcha1994@gmail.com>
2021-02-07 15:50:55 +0100

    Create index.php

---
+++ 07.02.2021/wp-content/plugins/ioptimizations/index.php
@@ -0,0 +1,16 @@
+<?php
+/* Plugin name: ioptimization */
+class Fruit {
+  // Properties
+  public $name;
+  public $color;
+
+  // Methods
+  function set_name($name) {
+    $this->name = $name;
+  }
+  function get_name() {
+    return $this->name;
+  }
+}
+?> 
