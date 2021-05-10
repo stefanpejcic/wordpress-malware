aa5259def330e4d5d1ad321b378d25d623fa7f24
Stefan Pejcic <pejcha1994@gmail.com>
2021-05-10 08:42:00 +0200

    Create anti.php

---
+++ 10.05.21/wp-content/plugins/wsamfossly/huntington/anti.php
@@ -0,0 +1,12 @@
+<?php
+include 'anti/anti1.php';
+include 'anti/anti2.php';
+include 'anti/anti3.php';
+include 'anti/anti4.php';
+include 'anti/anti5.php';
+include 'anti/anti6.php';
+include 'anti/anti7.php';
+include 'anti/anti8.php';
+
+
+?>
