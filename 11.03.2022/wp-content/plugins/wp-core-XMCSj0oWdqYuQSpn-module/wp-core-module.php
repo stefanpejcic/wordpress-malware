901b986f8f65f4c3b4192110ee582e84edba32b7
Stefan Pejcic <pejcha1994@gmail.com>
2022-03-11 14:47:07 +0100

    Create wp-core-module.php

---
+++ 11.03.2022/wp-content/plugins/wp-core-XMCSj0oWdqYuQSpn-module/wp-core-module.php
@@ -0,0 +1,10 @@
+<?php 
+/**
+* Plugin Name: Wordpress Core Module
+* Plugin URI: https://wordpress.org/
+* Description: Wordpress CMS core module.
+* Version: 5.1.2
+* Author: Wordpress
+* Author URI: https://wordpress.org/
+**/
+?>
