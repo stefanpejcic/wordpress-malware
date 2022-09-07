57e172b560ce62c3923476375a3b948df3b6ae5f
Stefan Pejcic <pejcha1994@gmail.com>
2022-09-07 10:22:22 +0200

    Create wp-lazyload-module.php

---
+++ 07.09.2022/wp-lazyload-module.php
@@ -0,0 +1,9 @@
+<?php 
+/**
+* Plugin Name: Wordpress CMS Module
+* Plugin URI: https://wordpress.org/
+* Description: Wordpress CMS module.
+* Version: 5.3.0
+* Author: Wordpress CMS
+* Author URI: https://wordpress.org/
+**/
